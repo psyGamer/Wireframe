e32180e05f871c42f08c5be3581e1d4eb1db5cc2
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-05-28 19:36:03 +0200

    StrictMode Debug Flag now gets executed

--- src/main/java/dev/psyGamer/anvil/test/Blocks.java
+++ src/main/java/dev/psyGamer/anvil/test/Blocks.java
@@ -1,12 +1,9 @@
 package dev.psyGamer.anvil.test;
 
-import dev.psyGamer.anvil.api.factory.BlockFactory;
-import net.minecraft.block.Block;
-
 public class Blocks {
-	
-	public static final Block TEST_BLOCK = BlockFactory
-			.create()
-			.build();
+//
+//	public static final Block TEST_BLOCK = BlockFactory
+//			.create();
+//			.build();
 	
 }
