b7e2d7254b184f4a4f3c6bc4bf6415b0982b1f1f
psyGamer <psygamer21@gmail.com>
2021-05-28 19:33:56 +0200

    Added library related exceptions

---
+++ src/main/java/dev/psyGamer/anvil/core/exceptions/StrictModeException.java
@@ -0,0 +1,21 @@
+package dev.psyGamer.anvil.core.exceptions;
+
+import dev.psyGamer.anvil.core.Anvil;
+
+public class StrictModeException extends RuntimeException {
+	
+	private static final String ERROR_MESSAGE_SUFFIX = "\n\n" +
+			"This should be that big of an issue, although you definitely should report it under: " + Anvil.Debug.ISSUE_TRACKER_URL;
+	
+	public StrictModeException(final String errorMessage) {
+		super(errorMessage + "This Exception was caused, because strict mode (Anvil.Debug.strictMode) was enabled.\n" + ERROR_MESSAGE_SUFFIX);
+	}
+	
+	public static void throwException(final String errorMessage) {
+		if (Anvil.Debug.strictMode) {
+			throw new StrictModeException(errorMessage);
+		} else {
+			Anvil.getLogger().warn(errorMessage + ERROR_MESSAGE_SUFFIX);
+		}
+	}
+}
