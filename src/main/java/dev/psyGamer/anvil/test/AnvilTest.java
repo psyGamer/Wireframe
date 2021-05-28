6380cfebb2e433de8dbda348dd71edf9e82a6c2f
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-05-28 18:20:43 +0200

    Removed implementation from library branch

--- src/main/java/dev/psyGamer/anvil/test/AnvilTest.java
+++ src/main/java/dev/psyGamer/anvil/test/AnvilTest.java
@@ -16,20 +16,26 @@ public class AnvilTest {
 	public static final String MOD_ID = "anvil";
 	public static final String MOD_NAME = "Anvil";
 	public static final String VERSION = "0.1";
-	
 	/**
 	 * This is the instance of your mod as created by Forge. It will never be null.
 	 */
 	@Mod.Instance(MOD_ID)
 	public static AnvilTest INSTANCE;
 	
+	public AnvilTest() {
+		Anvil.setup();
+	}
+	
+	public static void main(final String[] args) {
+		Anvil.setup();
+	}
 	
 	/**
 	 * This is the first initialization event. Register tile entities here.
 	 * The registry events below will have fired prior to entry to this method.
 	 */
 	@Mod.EventHandler
-	public void preInit(FMLPreInitializationEvent event) {
+	public void preInit(final FMLPreInitializationEvent event) {
 		Anvil.preInit(event);
 	}
 	
@@ -37,7 +43,7 @@ public class AnvilTest {
 	 * This is the second initialization event. Register custom recipes
 	 */
 	@Mod.EventHandler
-	public void init(FMLInitializationEvent event) {
+	public void init(final FMLInitializationEvent event) {
 		Anvil.init(event);
 	}
 	
@@ -45,7 +51,7 @@ public class AnvilTest {
 	 * This is the final initialization event. Register actions from other mods here
 	 */
 	@Mod.EventHandler
-	public void postInit(FMLPostInitializationEvent event) {
+	public void postInit(final FMLPostInitializationEvent event) {
 		Anvil.postInit(event);
 	}
 }
