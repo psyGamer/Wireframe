c640d86282f76e44ceebecd9b225d7e10985f9a0
psyGamer <psygamer21@gmail.com>
2021-05-28 21:45:43 +0200

    Renamed Anvil core class to AnvilCore

--- src/main/java/dev/psyGamer/anvil/core/exceptions/LibraryException.java
+++ src/main/java/dev/psyGamer/anvil/core/exceptions/LibraryException.java
@@ -1,12 +1,12 @@
 package dev.psyGamer.anvil.core.exceptions;
 
-import dev.psyGamer.anvil.core.Anvil;
+import dev.psyGamer.anvil.core.AnvilCore;
 
 public class LibraryException extends RuntimeException {
 	
 	public LibraryException(final String errorMessage) {
 		super("A fatal exception has occurred in the Anvil library!\n" +
-				"Please report this issue under: " + Anvil.Debug.ISSUE_TRACKER_URL +
+				"Please report this issue under: " + AnvilCore.Debug.ISSUE_TRACKER_URL +
 				"\n\nError message: " + errorMessage);
 	}
 }
