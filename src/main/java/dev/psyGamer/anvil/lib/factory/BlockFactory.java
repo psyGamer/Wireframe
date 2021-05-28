dd42d1301f2ce75b402e92ea9d7980a2ef201c1e
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-05-28 21:47:13 +0200

    Removed HasStaticMember due to it being useless

--- src/main/java/dev/psyGamer/anvil/lib/factory/BlockFactory.java
+++ src/main/java/dev/psyGamer/anvil/lib/factory/BlockFactory.java
@@ -1,15 +1,14 @@
 package dev.psyGamer.anvil.lib.factory;
 
-import dev.psyGamer.anvil.core.util.HasStaticMember;
+import dev.psyGamer.anvil.core.VersionHandler;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 
-@HasStaticMember
 public abstract class BlockFactory implements IFactory<Block> {
 	
 	public static BlockFactory create(final String blockName, final Material material, final CreativeTabs creativeTab) {
-		return null;
+		return (BlockFactory) VersionHandler.executeVersionedMethod(blockName, material, creativeTab);
 	}
 	
 }
