801488a7b5c84c1b3acfcabbe273ab12feedc7cb
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-05-29 13:17:29 +0200

    Removed testing implementation

--- src/main/java/dev/psyGamer/anvil/lib/registry/ItemRegistry.java
+++ src/main/java/dev/psyGamer/anvil/lib/registry/ItemRegistry.java
@@ -3,20 +3,9 @@ package dev.psyGamer.anvil.lib.registry;
 import dev.psyGamer.anvil.core.version.MinecraftVersion;
 import dev.psyGamer.anvil.core.version.RequireImplementation;
 import dev.psyGamer.anvil.core.version.SupportedVersion;
-import dev.psyGamer.anvil.core.version.VersionHandler;
-import net.minecraft.item.Item;
-
-import java.util.List;
 
 @RequireImplementation
 @SupportedVersion(MinecraftVersion.v12)
 public abstract class ItemRegistry {
-	
-	public static String registerItems(final List<Item> items) {
-		return (String) VersionHandler.executeOverloadedVersionedMethod(new Class[] { List.class }, items);
-	}
-	
-	public static String registerItems(final Item items) {
-		return (String) VersionHandler.executeOverloadedVersionedMethod(new Class[] { Item.class }, items);
-	}
+
 }
