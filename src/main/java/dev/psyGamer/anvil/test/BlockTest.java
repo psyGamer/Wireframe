50b3d4ae9c73cefd98419ce449af270a151ddb83
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-06-08 17:46:36 +0200

    Added test class

---
+++ src/main/java/dev/psyGamer/anvil/test/BlockTest.java
@@ -0,0 +1,16 @@
+package dev.psyGamer.anvil.test;
+
+import dev.psyGamer.anvil.lib.block.BlockFactory;
+import dev.psyGamer.anvil.lib.block.variants.BlockVariants;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+
+public class BlockTest {
+	
+	@BlockVariants
+	public static final Block TUFF_SANDSTONE = BlockFactory.create("tuff_sandstone")
+			.inheritFromBlock(Blocks.SANDSTONE)
+			.multiplyStrength(1.2f)
+			.build();
+	
+}
