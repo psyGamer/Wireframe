d211ba9f24e33c605ba7bb69b6237a2330ed2350
psyGamer <psygamer21@gmail.com>
2021-06-09 18:43:37 +0200

    Added implementation for block state data gen

--- src/main/java/dev/psyGamer/anvil/impl/v16/registry/BlockRegistryImpl16.java
+++ src/main/java/dev/psyGamer/anvil/impl/v16/registry/BlockRegistryImpl16.java
@@ -4,6 +4,8 @@ import dev.psyGamer.anvil.core.AnvilCore;
 import dev.psyGamer.anvil.lib.block.BlockWrapper;
 import dev.psyGamer.anvil.lib.registry.BlockRegistry;
 import net.minecraft.block.Block;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -13,15 +15,18 @@ public class BlockRegistryImpl16 extends BlockRegistry {
 	public static final DeferredRegister<Block> BLOCK_REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, AnvilCore.getModImplementation().MODID);
 	
 	@Override
-	public void registerBlocksToForge() {
+	@SubscribeEvent
+	public void registerBlocksToForge(final RegistryEvent.Register<Block> event) {
 		BLOCK_REGISTRY.register(FMLJavaModLoadingContext.get().getModEventBus());
 		
-		for (final BlockWrapper block : BLOCKS) {
-			BLOCK_REGISTRY.register(block.getRegistryName(), block.getBlockSupplier());
+		for (final BlockWrapper blockWrapper : blockWrappers) {
+			BLOCK_REGISTRY.register(blockWrapper.getRegistryName(), blockWrapper::getBlock);
+			blocks.add(blockWrapper.getBlock());
 			
-			if (block.hasBlockVariants()) {
-				for (final BlockWrapper blockVariant : block.getBlockVariants()) {
-					BLOCK_REGISTRY.register(blockVariant.getRegistryName(), blockVariant.getBlockSupplier());
+			if (blockWrapper.hasBlockVariants()) {
+				for (final BlockWrapper blockVariantWrapper : blockWrapper.getBlockVariants()) {
+					BLOCK_REGISTRY.register(blockVariantWrapper.getRegistryName(), blockVariantWrapper::getBlock);
+					blocks.add(blockVariantWrapper.getBlock());
 				}
 			}
 		}
