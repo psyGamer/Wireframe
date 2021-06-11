516aa51564e00a6230144376db2b36b1faaff142
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-06-11 15:37:03 +0200

    Added a getter for the mod implementations

--- src/main/java/dev/psyGamer/anvil/core/ModImplementation.java
+++ src/main/java/dev/psyGamer/anvil/core/ModImplementation.java
@@ -2,12 +2,16 @@ package dev.psyGamer.anvil.core;
 
 public class ModImplementation <T> {
 	public final String modID;
-	public final T modInstance;
+	public final String rootPackage;
+	
 	public final Class<? extends T> modClass;
+	public final T modInstance;
 	
 	public ModImplementation(final String modID, final T modInstance, final Class<? extends T> modClass) {
 		this.modID = modID;
-		this.modInstance = modInstance;
+		this.rootPackage = modClass.getPackage().getName();
+		
 		this.modClass = modClass;
+		this.modInstance = modInstance;
 	}
 }
