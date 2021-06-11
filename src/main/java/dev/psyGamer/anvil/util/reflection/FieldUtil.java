b81af7a629a93f46eeabeec92409c4aa153fedda
psyGamer <psygamer21@gmail.com>
2021-06-11 15:25:28 +0200

    Fixed FieldUtil not returning Field object instead of value

--- src/main/java/dev/psyGamer/anvil/util/reflection/FieldUtil.java
+++ src/main/java/dev/psyGamer/anvil/util/reflection/FieldUtil.java
@@ -1,14 +1,20 @@
 package dev.psyGamer.anvil.util.reflection;
 
-import lombok.SneakyThrows;
+import java.lang.reflect.Field;
 
 public class FieldUtil {
 	
 	public static Object getField(final Object obj, final String fieldName) {
 		try {
-			return obj.getClass().getField(fieldName).getType();
+			final Field field = obj.getClass().getField(fieldName);
+			
+			field.setAccessible(true);
+			
+			return field.get(obj);
 		} catch (final NoSuchFieldException ex) {
 			throw new IllegalArgumentException("Field " + fieldName + " was not found on " + obj);
+		} catch (final IllegalAccessException ex) {
+			throw new IllegalArgumentException("Field " + fieldName + " could not be accessed in " + obj);
 		}
 	}
 }
