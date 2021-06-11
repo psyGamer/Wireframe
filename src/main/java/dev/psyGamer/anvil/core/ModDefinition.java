cef11de74cf17df5e35c3f0b9abbb4d7aa7b13ad
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-06-11 15:40:27 +0200

    Renamed ModImplementation to ModDefinition and Dependant

--- src/main/java/dev/psyGamer/anvil/core/ModDefinition.java
+++ src/main/java/dev/psyGamer/anvil/core/ModDefinition.java
@@ -1,13 +1,13 @@
 package dev.psyGamer.anvil.core;
 
-public class ModImplementation <T> {
+public class ModDefinition <T> {
 	public final String modID;
 	public final String rootPackage;
 	
 	public final Class<? extends T> modClass;
 	public final T modInstance;
 	
-	public ModImplementation(final String modID, final T modInstance, final Class<? extends T> modClass) {
+	public ModDefinition(final String modID, final T modInstance, final Class<? extends T> modClass) {
 		this.modID = modID;
 		this.rootPackage = modClass.getPackage().getName();
 		
