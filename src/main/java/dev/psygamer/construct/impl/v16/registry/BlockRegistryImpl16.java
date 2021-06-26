b316662ebada3774c9f7f778d7a8dc955ae37656
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-06-26 09:49:26 +0200

    Cleaned up BlockRegistry
    - Renamed addBlockWrapper -> register
    - Moved logging out of registerBlockWrapper
    - Using correct FMLJavaModContainer

--- src/main/java/dev/psygamer/construct/impl/v16/registry/BlockRegistryImpl16.java
+++ src/main/java/dev/psygamer/construct/impl/v16/registry/BlockRegistryImpl16.java
@@ -5,6 +5,7 @@ import dev.psygamer.construct.impl.common.registry.CommonBlockRegistry;
 import dev.psygamer.construct.lib.block.BlockWrapper;
 import net.minecraft.block.Block;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -18,40 +19,47 @@ public class BlockRegistryImpl16 extends CommonBlockRegistry {
 	public static final Map<String, DeferredRegister<Block>> BLOCK_REGISTRIES = new HashMap<>();
 	
 	@Override
-	@SubscribeEvent
+	@SubscribeEvent(priority = EventPriority.HIGHEST)
 	public void registerBlocksToForge(final RegistryEvent.Register<Block> event) {
 		setupRegistries();
 		
 		BLOCK_REGISTRIES.forEach((namespace, registry) -> blockWrappers.forEach(blockWrapper -> {
-			registerBlockWrapper(blockWrapper, registry, false);
+			registerBlockWrapper(blockWrapper, registry);
 			
-			blockWrapper.getBlockVariants().forEach(wrapper -> registerBlockWrapper(wrapper, registry, true));
+			ConstructCore.LOGGER.info(
+					String.format("Successfully registered block %s:%s",
+							blockWrapper.getNamespace().get(),
+							blockWrapper.getRegistryName()
+					)
+			);
+			
+			if (blockWrapper.hasBlockVariants()) {
+				blockWrapper.getBlockVariants().forEach(wrapper -> {
+					registerBlockWrapper(wrapper, registry);
+					
+					ConstructCore.LOGGER.info(
+							String.format("   |=> Successfully registered block variant%s:%s",
+									blockWrapper.getNamespace().get(),
+									blockWrapper.getRegistryName()
+							)
+					);
+				});
+			}
 		}));
 	}
 	
-	protected void registerBlockWrapper(final BlockWrapper blockWrapper, final DeferredRegister<Block> registry, final boolean isVariant) {
+	protected void registerBlockWrapper(final BlockWrapper blockWrapper, final DeferredRegister<Block> registry) {
+		blockWrapper.getBlock().setRegistryName(
+				blockWrapper.getNamespace().get(),
+				blockWrapper.getRegistryName()
+		);
+		
 		registry.register(
 				blockWrapper.getRegistryName(),
 				blockWrapper::getBlock
 		);
 		
 		blocks.add(blockWrapper.getBlock());
-		
-		if (isVariant) {
-			ConstructCore.LOGGER.info(
-					String.format("   |=> Successfully registered block variant -> %s:%s",
-							blockWrapper.getNamespace(),
-							blockWrapper.getRegistryName()
-					)
-			);
-		} else {
-			ConstructCore.LOGGER.info(
-					String.format("Successfully registered block -> %s:%s",
-							blockWrapper.getNamespace(),
-							blockWrapper.getRegistryName()
-					)
-			);
-		}
 	}
 	
 	protected void setupRegistries() {
@@ -60,7 +68,7 @@ public class BlockRegistryImpl16 extends CommonBlockRegistry {
 			
 			if (!BLOCK_REGISTRIES.containsKey(namespace)) {
 				BLOCK_REGISTRIES.put(namespace, DeferredRegister.create(ForgeRegistries.BLOCKS, namespace));
-				BLOCK_REGISTRIES.get(namespace).register(FMLJavaModLoadingContext.get().getModEventBus());
+				BLOCK_REGISTRIES.get(namespace).register(ConstructCore.Util.getCurrentDependant().getModLoadingContext().getModEventBus());
 			}
 		}
 	}
