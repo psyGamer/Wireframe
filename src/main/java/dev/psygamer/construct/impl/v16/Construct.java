2dd9b79389b87039773798d2a2d4e64ef9d101e1
psyGamer <psygamer21@gmail.com>
2021-06-26 09:46:22 +0200

    Fixed using wrong event bus

--- src/main/java/dev/psygamer/construct/impl/v16/Construct.java
+++ src/main/java/dev/psygamer/construct/impl/v16/Construct.java
@@ -2,16 +2,16 @@ package dev.psygamer.construct.impl.v16;
 
 import dev.psygamer.construct.core.ConstructCore;
 import dev.psygamer.construct.impl.v16.registry.BlockRegistryImpl16;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(ConstructCore.MODID)
 public class Construct {
 	
 	public Construct() {
-		final IEventBus forgeEventBus = MinecraftForge.EVENT_BUS;
+		final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		
-		forgeEventBus.register(new BlockRegistryImpl16());
+		modEventBus.register(new BlockRegistryImpl16());
 	}
 }
