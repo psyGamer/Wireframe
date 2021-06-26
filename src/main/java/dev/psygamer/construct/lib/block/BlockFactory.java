c06a48ce1389ba60a5aa393b7eac5b866ebdfb3a
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-06-26 13:06:47 +0200

    Removed now redundant casts

--- src/main/java/dev/psygamer/construct/lib/block/BlockFactory.java
+++ src/main/java/dev/psygamer/construct/lib/block/BlockFactory.java
@@ -1,8 +1,8 @@
 package dev.psygamer.construct.lib.block;
 
+import dev.psygamer.construct.core.version.ImplementationHandler;
 import dev.psygamer.construct.lib.block.properties.HarvestLevel;
 import dev.psygamer.construct.lib.util.ICloneable;
-import dev.psygamer.construct.core.version.ImplementationHandler;
 import dev.psygamer.construct.core.version.MinecraftVersion;
 import dev.psygamer.construct.core.version.SupportedSince;
 import dev.psygamer.construct.lib.util.IFactory;
@@ -36,7 +36,7 @@ public interface BlockFactory extends IFactory<Block>, ICloneable<BlockFactory> 
 	 * @since 1.0
 	 */
 	static BlockFactory create(final String blockName) {
-		return (BlockFactory) ImplementationHandler.executeImplementation(blockName);
+		return ImplementationHandler.executeImplementation(blockName);
 	}
 	
 	/**
@@ -50,7 +50,7 @@ public interface BlockFactory extends IFactory<Block>, ICloneable<BlockFactory> 
 	 * @since 1.0
 	 */
 	static BlockFactory create(final String blockName, final Material material) {
-		return (BlockFactory) ImplementationHandler.executeImplementation(blockName, material);
+		return ImplementationHandler.executeImplementation(blockName, material);
 	}
 	
 	/**
@@ -65,7 +65,7 @@ public interface BlockFactory extends IFactory<Block>, ICloneable<BlockFactory> 
 	 * @since 1.0
 	 */
 	static BlockFactory create(final String blockName, final Material material, final ItemGroup group) {
-		return (BlockFactory) ImplementationHandler.executeImplementation(blockName, material, group);
+		return ImplementationHandler.executeImplementation(blockName, material, group);
 	}
 	
 	BlockFactory multiplyHardness(final float factor);
--- src/main/java/dev/psygamer/construct/lib/registry/BlockRegistry.java
+++ src/main/java/dev/psygamer/construct/lib/registry/BlockRegistry.java
@@ -10,7 +10,6 @@ import net.minecraftforge.event.RegistryEvent;
 import java.util.List;
 
 
-@SuppressWarnings("unchecked")
 @SupportedSince(MinecraftVersion.v16)
 public interface BlockRegistry {
 	
@@ -19,11 +18,11 @@ public interface BlockRegistry {
 	}
 	
 	static List<Block> getBlocks() {
-		return (List<Block>) ImplementationHandler.executeImplementation();
+		return ImplementationHandler.executeImplementation();
 	}
 	
 	static List<BlockWrapper> getBlockWrappers() {
-		return (List<BlockWrapper>) ImplementationHandler.executeImplementation();
+		return ImplementationHandler.executeImplementation();
 	}
 	
 	void registerBlocksToForge(RegistryEvent.Register<Block> event);
