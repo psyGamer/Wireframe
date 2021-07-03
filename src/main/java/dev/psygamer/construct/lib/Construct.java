fdb249780347f6ba85626060bb4d2a442fd11f83
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-07-03 11:12:28 +0200

    Added implementation to Construct

--- src/main/java/dev/psygamer/construct/lib/Construct.java
+++ src/main/java/dev/psygamer/construct/lib/Construct.java
@@ -1,14 +1,16 @@
 package dev.psygamer.construct.lib;
 
+import dev.psygamer.construct.core.version.ImplementationHandler;
+
 public interface Construct {
 	
 	final class Core {
-		public static Object executeImplementation(final Object... parameters) {
-			return null; // Actual Implementation in impl branch
+		public static <T> T executeImplementation(final Object... parameters) {
+			return ImplementationHandler.executeImplementation(parameters);
 		}
 		
-		public static Object executeSpecificImplementation(final Class<?>[] parameterTypes, final Object... parameters) {
-			return null; // Actual Implementation in impl branch
+		public static <T> T executeSpecificImplementation(final Class<?>[] parameterTypes, final Object... parameters) {
+			return ImplementationHandler.executeSpecificImplementation(parameterTypes, parameters);
 		}
 	}
 }
