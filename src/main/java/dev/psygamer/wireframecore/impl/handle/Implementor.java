9e605380e51ccc259fe7fa5f3798549467747652
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-07-22 17:55:17 +0200

    Fixed bugs in Implmentor
    - Now checks if the API Class is assignable from
    - Now checks for >= instead of <= when filtering out newer versions
    - Added a mapping method to map implementation method to API method

--- src/main/java/dev/psygamer/wireframecore/impl/handle/Implementor.java
+++ src/main/java/dev/psygamer/wireframecore/impl/handle/Implementor.java
@@ -47,19 +47,41 @@ public final class Implementor {
 				.orElseThrow(() -> new NoInvokerFoundException("Class part of the Wireframe library classes"));
 		
 		final Class<?> libraryClass = ClassUtil.getClassFromStackTraceElement(invoker);
-		final Method libraryMethod = MethodUtil.getStaticMethod(libraryClass, invoker.getMethodName(), parameterTypes);
+		final Method libraryMethod = MethodUtil.getStaticMethod(libraryClass, invoker.getMethodName(), mapToAPIClasses(parameterTypes));
 		
 		return new Implementor(libraryMethod);
+	}
+	
+	private static Class<?>[] mapToAPIClasses(final Class<?>[] classes) {
+		return Arrays.stream(classes)
+				.map(clazz -> {
+					if (!WireframePackages.isImplClass(clazz)) {
+						return clazz;
+					}
+					
+					while (Arrays.stream(clazz.getInterfaces())
+							.filter(WireframePackages::isAPIClass)
+							.count() != 1
+					) {
+						clazz = clazz.getSuperclass();
+					}
+					
+					return Arrays.stream(clazz.getInterfaces())
+							.filter(WireframePackages::isAPIClass)
+							.findFirst()
+							.orElse(clazz);
+				})
+				.toArray(Class[]::new);
 	}
 	
 	protected static Method evaluateImplementationMethod(final Method apiMethod) {
 		final Class<?> apiClass = apiMethod.getDeclaringClass();
 		
 		return ClassUtil.getClasses(WireframePackages.IMPL_PACKAGE).stream()
-				.filter(clazz -> clazz.isAssignableFrom(apiClass))
+				.filter(apiClass::isAssignableFrom)
 				.filter(clazz -> clazz.isAnnotationPresent(ImplementationVersion.class))
 				.filter(clazz -> MinecraftVersion.getCurrentVersion().compareTo( // Check if the impl version is <= the current version
-						clazz.getAnnotation(ImplementationVersion.class).value()) <= 0)
+						clazz.getAnnotation(ImplementationVersion.class).value()) >= 0)
 				.sorted((classA, classB) -> { // Sort from newest to newest to oldest
 					final MinecraftVersion versionA = classA.getAnnotation(ImplementationVersion.class).value();
 					final MinecraftVersion versionB = classB.getAnnotation(ImplementationVersion.class).value();
