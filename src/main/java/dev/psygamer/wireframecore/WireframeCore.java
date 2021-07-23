cc39fd8f2d0c966c9ffcf567912935d709e3e9ad
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-07-23 20:38:37 +0200

    Moved mod class to WireframeCore

--- src/main/java/dev/psygamer/wireframecore/WireframeCore.java
+++ src/main/java/dev/psygamer/wireframecore/WireframeCore.java
@@ -1,15 +1,19 @@
 package dev.psygamer.wireframecore;
 
 import dev.psygamer.wireframecore.dependant.DependantsHandler;
+import dev.psygamer.wireframecore.event.EventRegistrator;
 import dev.psygamer.wireframecore.exceptions.FrameworkException;
 import dev.psygamer.wireframecore.namespace.NamespaceHandler;
 
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLConstructModEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+@Mod(WireframeCore.MODID)
 public final class WireframeCore {
 	
 	public static final Logger LOGGER = LogManager.getLogger("Wireframe");
