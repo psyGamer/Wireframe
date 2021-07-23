99ac14a985cc4d044f307a8c544af6edbce01d7e
psyGamer <psygamer21@gmail.com>
2021-07-23 20:37:27 +0200

    Added Implementor.findClass
    - Returns the newest implementation class

--- src/main/java/dev/psygamer/wireframecore/impl/handle/Implementor.java
+++ src/main/java/dev/psygamer/wireframecore/impl/handle/Implementor.java
@@ -1,25 +1,21 @@
 package dev.psygamer.wireframecore.impl.handle;
 
-import dev.psygamer.wireframecore.WireframePackages;
-import dev.psygamer.wireframecore.exceptions.FrameworkException;
-import dev.psygamer.wireframecore.impl.ImplementationVersion;
-import dev.psygamer.wireframecore.impl.MinecraftVersion;
-import dev.psygamer.wireframecore.impl.handle.exceptions.NoInvokerFoundException;
-import dev.psygamer.wireframecore.impl.handle.exceptions.NoMethodImplementorFoundException;
-
 import dev.psygamer.wireframe.util.reflection.ClassUtil;
 import dev.psygamer.wireframe.util.reflection.MethodUtil;
 import dev.psygamer.wireframe.util.reflection.ObjectUtil;
 
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
+import dev.psygamer.wireframecore.WireframePackages;
+import dev.psygamer.wireframecore.impl.ImplementationVersion;
+import dev.psygamer.wireframecore.impl.MinecraftVersion;
+import dev.psygamer.wireframecore.impl.handle.exceptions.NoInvokerFoundException;
+import dev.psygamer.wireframecore.impl.handle.exceptions.NoClassImplementorFoundException;
+import dev.psygamer.wireframecore.impl.handle.exceptions.NoMethodImplementorFoundException;
 
-import java.util.concurrent.ExecutionException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Objects;
 import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 public final class Implementor {
 	
@@ -52,6 +48,12 @@ public final class Implementor {
 		return new Implementor(libraryMethod);
 	}
 	
+	public static Class<?> findClass(final Class<?> apiClass) {
+		return getImplementationClassStream(apiClass)
+				.findFirst()
+				.orElseThrow(() -> new NoClassImplementorFoundException(apiClass));
+	}
+	
 	private static Class<?>[] mapToAPIClasses(final Class<?>[] classes) {
 		return Arrays.stream(classes)
 				.map(clazz -> {
@@ -77,17 +79,7 @@ public final class Implementor {
 	protected static Method evaluateImplementationMethod(final Method apiMethod) {
 		final Class<?> apiClass = apiMethod.getDeclaringClass();
 		
-		return ClassUtil.getClasses(WireframePackages.IMPL_PACKAGE).stream()
-				.filter(apiClass::isAssignableFrom)
-				.filter(clazz -> clazz.isAnnotationPresent(ImplementationVersion.class))
-				.filter(clazz -> MinecraftVersion.getCurrentVersion().compareTo( // Check if the impl version is <= the current version
-						clazz.getAnnotation(ImplementationVersion.class).value()) >= 0)
-				.sorted((classA, classB) -> { // Sort from newest to newest to oldest
-					final MinecraftVersion versionA = classA.getAnnotation(ImplementationVersion.class).value();
-					final MinecraftVersion versionB = classB.getAnnotation(ImplementationVersion.class).value();
-					
-					return versionA.compareTo(versionB);
-				})
+		return getImplementationClassStream(apiClass)
 				.map(clazz -> MethodUtil.getStaticMethod( // Map to implementation method
 						clazz,
 						apiMethod.getName(),
@@ -98,40 +90,28 @@ public final class Implementor {
 		
 	}
 	
+	protected static Stream<Class<?>> getImplementationClassStream(final Class<?> apiClass) {
+		return ClassUtil.getClasses(WireframePackages.IMPL_PACKAGE).stream()
+				.filter(apiClass::isAssignableFrom)
+				.filter(clazz -> clazz.isAnnotationPresent(ImplementationVersion.class))
+				.filter(clazz -> MinecraftVersion.getCurrentVersion().compareTo( // Check if the impl version is <= the current version
+						clazz.getAnnotation(ImplementationVersion.class).value()) >= 0)
+				.sorted((classA, classB) -> { // Sort from newest to newest to oldest
+					final MinecraftVersion versionA = classA.getAnnotation(ImplementationVersion.class).value();
+					final MinecraftVersion versionB = classB.getAnnotation(ImplementationVersion.class).value();
+					
+					return versionB.compareTo(versionA);
+				});
+	}
+	
 	@SuppressWarnings("unchecked")
 	public <R> R run(final Object... parameters) {
-		final Method implementedMethod = Cache.getCachedMethod(this.apiMethod);
+		final Method implementedMethod = ImplementorCache.getCachedMethod(this.apiMethod);
 		
 		try {
 			return (R) implementedMethod.invoke(null, parameters);
 		} catch (IllegalAccessException | InvocationTargetException ex) {
 			throw new NoMethodImplementorFoundException(this.apiMethod, ex);
-		}
-	}
-	
-	private static final class Cache {
-		private static final LoadingCache<Method, Method> methodCache = CacheBuilder.newBuilder()
-				.maximumSize(300)
-				.build(
-						new CacheLoader<Method, Method>() {
-							
-							@Override
-							public Method load(final Method key) {
-								return Implementor.evaluateImplementationMethod(key);
-							}
-						}
-				);
-		
-		private static LoadingCache<Method, Method> getMethodCache() {
-			return methodCache;
-		}
-		
-		private static Method getCachedMethod(final Method apiMethod) {
-			try {
-				return methodCache.get(apiMethod);
-			} catch (final ExecutionException ex) {
-				throw new FrameworkException("Could not get implementation method for " + methodCache, ex);
-			}
 		}
 	}
 }
