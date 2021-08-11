1586c2d7904b4b944d859922a0bac7c8271f503c
psyGamer <psygamer21@gmail.com>
2021-08-11 17:14:23 +0200

    Added BlockProperties to BasicBlock

--- src/main/java/dev/psygamer/wireframe/api/block/BasicBlock.java
+++ src/main/java/dev/psygamer/wireframe/api/block/BasicBlock.java
@@ -1,18 +1,34 @@
 package dev.psygamer.wireframe.api.block;
 
+import dev.psygamer.wireframe.api.block.property.BlockProperty;
 import dev.psygamer.wireframe.core.impl.Instancer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class BasicBlock {
 	
-	private static final BasicBlock instance = Instancer.createInstance();
+	private static final BasicBlock INSTANCE = Instancer.createInstance();
 	
 	protected final String registryName;
+	protected final BlockProperties blockProperties;
 	
-	public BasicBlock(final String blockName, final BlockProperties properties) {
+	protected final List<BlockProperty<?>> blockStateProperties = new ArrayList<>();
+	
+	public BasicBlock(final String blockName, final BlockProperties blockProperties) {
 		this.registryName = blockName;
+		this.blockProperties = blockProperties;
 	}
 	
 	public String getRegistryName() {
 		return this.registryName;
+	}
+	
+	public BlockProperties getBlockProperties() {
+		return this.blockProperties;
+	}
+	
+	protected void registerBlockStateProperty(final BlockProperty<?> property) {
+		this.blockStateProperties.add(property);
 	}
 }
