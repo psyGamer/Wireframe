75ad4134af5a5dd38dfb1584bd8325df5b7f8dc5
psyGamer <psygamer21@gmail.com>
2021-08-11 17:05:20 +0200

    Removed BlockProperty from BlockStateDataProviderImpl16

--- src/main/java/dev/psygamer/wireframe/impl/v16/data/client/BlockStateDataProviderImpl16.java
+++ src/main/java/dev/psygamer/wireframe/impl/v16/data/client/BlockStateDataProviderImpl16.java
@@ -1,16 +1,14 @@
 package dev.psygamer.wireframe.impl.v16.data.client;
 
-import dev.psygamer.wireframe.core.impl.ImplementationVersion;
 import dev.psygamer.wireframe.core.impl.MinecraftVersion;
-import dev.psygamer.wireframe.api.registry.BlockRegistry;
-import dev.psygamer.wireframe.api.block.BlockWrapper;
-import dev.psygamer.wireframe.api.block.BlockProperty;
-import dev.psygamer.wireframe.api.data.client.BlockStateDataProvider;
+import dev.psygamer.wireframe.core.impl.ImplementationVersion;
 
-import net.minecraftforge.client.model.generators.ConfiguredModel;
+import dev.psygamer.wireframe.api.data.client.BlockStateDataProvider;
+import dev.psygamer.wireframe.api.block.BlockWrapper;
+import dev.psygamer.wireframe.api.registry.BlockRegistry;
+
 import net.minecraftforge.common.data.ExistingFileHelper;
 
-import net.minecraft.block.BlockState;
 import net.minecraft.data.DataGenerator;
 
 
@@ -24,20 +22,22 @@ public class BlockStateDataProviderImpl16 extends BlockStateDataProvider {
 	@Override
 	protected void registerStatesAndModels() {
 		for (final BlockWrapper blockWrapper : BlockRegistry.getBlockWrappers()) {
-			if (blockWrapper.getBlockProperties().size() == 0) {
-				simpleBlock(blockWrapper.getBlock());
-			} else {
-				for (final BlockState state : blockWrapper.getBlock().getStateDefinition().getPossibleStates()) {
-					ConfiguredModel.Builder<?> modelBuilder = ConfiguredModel.builder();
-					
-					for (final BlockProperty<? extends Comparable<?>> property : blockWrapper.getBlockProperties()) {
-						modelBuilder = property.applyBlockModelModification(
-								state.getValue(property),
-								modelBuilder
-						);
-					}
-				}
-			}
+//			simpleBlock(blockWrapper.getBlock());
+
+//			if (blockWrapper.getBlockProperties().size() == 0) {
+//				simpleBlock(blockWrapper.getBlock());
+//			} else {
+//				for (final BlockState state : blockWrapper.getBlock().getStateDefinition().getPossibleStates()) {
+//					ConfiguredModel.Builder<?> modelBuilder = ConfiguredModel.builder();
+//
+//					for (final BlockProperty<? extends Comparable<?>> property : blockWrapper.getBlockProperties()) {
+//						modelBuilder = property.applyBlockModelModification(
+//								state.getValue(property),
+//								modelBuilder
+//						);
+//					}
+//				}
+//			}
 		}
 	}
 }
