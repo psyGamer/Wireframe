9c5a064ae8e98e0ba3e29d0a0b94173f46c3e57e
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-08-12 11:50:15 +0200

    BlockRegistryImpl16 now uses BasicBlocks

--- src/main/java/dev/psygamer/wireframe/api/registry/BlockRegistry.java
+++ src/main/java/dev/psygamer/wireframe/api/registry/BlockRegistry.java
@@ -2,19 +2,13 @@ package dev.psygamer.wireframe.api.registry;
 
 import dev.psygamer.wireframe.api.block.BasicBlock;
 
-import dev.psygamer.wireframe.core.event.ModEventBusSubscriber;
 import dev.psygamer.wireframe.core.impl.Instancer;
 
 import com.google.common.collect.ImmutableList;
 
-@ModEventBusSubscriber
 public abstract class BlockRegistry {
 	
 	private static final BlockRegistry INSTANCE = Instancer.createInstance();
-	
-	public static BlockRegistry create(final String modID) {
-		return INSTANCE.createInstance(modID);
-	}
 	
 	public static void register(final BasicBlock block) {
 		INSTANCE.registerBasicBlock(block);
@@ -23,8 +17,6 @@ public abstract class BlockRegistry {
 	public static ImmutableList<BasicBlock> getBlocks() {
 		return INSTANCE.getBlocksBasicBlocks();
 	}
-	
-	protected abstract BlockRegistry createInstance(final String modID);
 	
 	protected abstract void registerBasicBlock(final BasicBlock block);
 	
