5d8105290763f3db99b89f6f13c1737c33394fbd
psyGamer <psygamer21@gmail.com>
2021-08-12 10:47:51 +0200

    Added CompiledBlockPropertyImpl16
    - Compiles BlockProperties into vanilla Properties

---
+++ src/main/java/dev/psygamer/wireframe/impl/v16/block/CompiledBlockPropertyImpl16.java
@@ -0,0 +1,58 @@
+package dev.psygamer.wireframe.impl.v16.block;
+
+import dev.psygamer.wireframe.api.block.state.property.BlockProperty;
+
+import dev.psygamer.wireframe.util.collection.FreezableHashMap;
+import dev.psygamer.wireframe.util.collection.FreezableMap;
+import net.minecraft.state.Property;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public class CompiledBlockPropertyImpl16 <T extends Comparable<T>> extends Property<T> {
+	
+	private static final FreezableMap<BlockProperty<? extends Comparable<?>>, Property<? extends Comparable<?>>>
+			blockPropertyCache = new FreezableHashMap<>();
+	
+	private final BlockProperty<T> blockProperty;
+	
+	protected CompiledBlockPropertyImpl16(final BlockProperty<T> blockProperty) {
+		super(blockProperty.getPropertyName(), (Class<T>) blockProperty.getDefaultValue().getClass());
+		
+		this.blockProperty = blockProperty;
+		
+		blockPropertyCache.put(blockProperty, this);
+	}
+	
+	public static Property<?> getCachedProperty(final BlockProperty<?> blockProperty) {
+		return blockPropertyCache.get(blockProperty);
+	}
+	
+	public static BlockProperty<?> getCachedBlockProperty(final Property<?> property) {
+		for (final BlockProperty<? extends Comparable<?>> blockProperty : blockPropertyCache.keySet()) {
+			final Property<?> value = getCachedProperty(blockProperty);
+			
+			if (value.equals(property))
+				return blockProperty;
+		}
+		
+		return null;
+	}
+	
+	@Override
+	public Collection<T> getPossibleValues() {
+		return this.blockProperty.getPossibleValues();
+	}
+	
+	@Override
+	public String getName(final T p_177702_1_) {
+		return this.blockProperty.getPropertyName();
+	}
+	
+	@Override
+	public Optional<T> getValue(final String valueName) {
+		final T value = this.blockProperty.getValue(valueName);
+		
+		return value == null ? Optional.empty() : Optional.of(value);
+	}
+}
