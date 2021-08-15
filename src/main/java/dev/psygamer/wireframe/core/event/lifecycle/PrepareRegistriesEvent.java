2dcb687469a8da075bb3c3cfac95849f77e372cc
psyGamer <psygamer21@gmail.com>
2021-08-15 21:52:03 +0200

    Added PrepareRegistriesEvent
    - Will be used to automatically register blocks, items, etc.

---
+++ src/main/java/dev/psygamer/wireframe/core/event/lifecycle/PrepareRegistriesEvent.java
@@ -0,0 +1,5 @@
+package dev.psygamer.wireframe.core.event.lifecycle;
+
+import dev.psygamer.wireframe.core.event.IWireframeMarkerEvent;
+
+public class PrepareRegistriesEvent implements IWireframeMarkerEvent { }
