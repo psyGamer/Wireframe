f4323ab31d9089a17820fd749b4772f79a71806f
psyGamer <psygamer21@gmail.com>
2021-09-10 12:07:35 +0200

    Added wrapper for Rarity

---
+++ src/main/java/dev/psygamer/wireframe/item/Rarity.java
@@ -0,0 +1,19 @@
+package dev.psygamer.wireframe.item;
+
+public enum Rarity {
+	
+	COMMON(net.minecraft.item.Rarity.COMMON),
+	UNCOMMON(net.minecraft.item.Rarity.UNCOMMON),
+	RARE(net.minecraft.item.Rarity.RARE),
+	EPIC(net.minecraft.item.Rarity.EPIC);
+	
+	private final net.minecraft.item.Rarity internal;
+	
+	Rarity(final net.minecraft.item.Rarity internal) {
+		this.internal = internal;
+	}
+	
+	public net.minecraft.item.Rarity getInternal() {
+		return this.internal;
+	}
+}
