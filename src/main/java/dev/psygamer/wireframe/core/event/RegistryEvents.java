6c973c4b79d1d3527a38d5e6341ada263812b1a8
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-09-13 19:09:43 +0200

    Moved old event system

--- src/main/java/dev/psygamer/wireframe/core/event/RegistryEvents.java
+++
@@ -1,8 +0,0 @@
-package dev.psygamer.wireframe.core.event;
-
-public class RegistryEvents {
-	
-	public static final class Prepare extends WireframeMarkerEvent { }
-	public static final class Freeze  extends WireframeMarkerEvent { }
-	
-}
--- src/main/java/dev/psygamer/wireframe/core/event/WireframeMarkerEvent.java
+++
@@ -1,5 +0,0 @@
-package dev.psygamer.wireframe.core.event;
-
-import net.minecraftforge.eventbus.api.Event;
-
-public abstract class WireframeMarkerEvent extends Event implements IWireframeEvent { }
