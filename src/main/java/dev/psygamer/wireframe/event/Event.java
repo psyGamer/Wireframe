7613df2e6f12cc00b64522b45f32f961622a585e
psyGamer <psygamer21@gmail.com>
2021-09-13 17:57:58 +0200

    Added a basic event system

---
+++ src/main/java/dev/psygamer/wireframe/event/Event.java
@@ -0,0 +1,27 @@
+package dev.psygamer.wireframe.event;
+
+public abstract class Event {
+	
+	private boolean canceled = false;
+	
+	public enum Priority {
+		HIGHEST, HIGH, NORMAL, LOW, LOWEST
+	}
+	
+	public abstract boolean isCancelable();
+	
+	public boolean isCanceled() {
+		return this.canceled;
+	}
+	
+	public void setCanceled(final boolean canceled) {
+		if (!isCancelable()) {
+			throw new UnsupportedOperationException(
+					"Attempted to call Event#setCanceled() on a non-cancelable event of type: "
+							+ this.getClass().getCanonicalName()
+			);
+		}
+		
+		this.canceled = canceled;
+	}
+}
---
+++ src/main/java/dev/psygamer/wireframe/event/EventSubscriber.java
@@ -0,0 +1,13 @@
+package dev.psygamer.wireframe.event;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface EventSubscriber {
+	
+	Event.Priority priority();
+}
