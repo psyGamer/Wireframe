9befd2e19a475ae21d63ac8152629a118ea02442
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-09-13 16:05:58 +0200

    Moved mod class to Wireframe

--- src/main/java/dev/psygamer/wireframe/Wireframe.java
+++ src/main/java/dev/psygamer/wireframe/Wireframe.java
@@ -1,4 +1,23 @@
 package dev.psygamer.wireframe;
 
-public interface Wireframe {
+import dev.psygamer.wireframe.core.WireframeCore;
+import dev.psygamer.wireframe.core.eventbus.EventBusRegistrator;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLConstructModEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(WireframeCore.MODID)
+public class Wireframe {
+	
+	public Wireframe() {
+		final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		
+		modEventBus.addListener(this::onModConstruct);
+	}
+	
+	private void onModConstruct(final FMLConstructModEvent event) {
+		EventBusRegistrator.registerModLoadingEventBuses();
+		EventBusRegistrator.registerWireframeEventBuses();
+	}
 }
--- src/main/java/dev/psygamer/wireframe/core/WireframeCore.java
+++ src/main/java/dev/psygamer/wireframe/core/WireframeCore.java
@@ -2,30 +2,21 @@ package dev.psygamer.wireframe.core;
 
 import dev.psygamer.wireframe.core.dependant.Dependant;
 import dev.psygamer.wireframe.core.dependant.Namespace;
-import dev.psygamer.wireframe.core.eventbus.EventBusRegistrator;
 import dev.psygamer.wireframe.core.eventbus.WireframeEventBus;
 import dev.psygamer.wireframe.core.exceptions.FrameworkException;
 
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLConstructModEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.eventbus.api.IEventBus;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(WireframeCore.MODID)
 public final class WireframeCore {
 	public static final Logger LOGGER = LogManager.getLogger("Wireframe");
 	public static final String MODID = "wireframe";
 	
 	public static final IEventBus EVENT_BUS = WireframeEventBus.create();
-	
-	public WireframeCore() {
-		final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-		
-		modEventBus.addListener(this::onModConstruct);
-	}
 	
 	public static void register(final Class<?> modClass, final FMLJavaModLoadingContext modLoadingContext) {
 		if (!modClass.isAnnotationPresent(Mod.class)) {
@@ -42,11 +33,4 @@ public final class WireframeCore {
 				modClass.getPackage().getName()
 		);
 	}
-	
-	private void onModConstruct(final FMLConstructModEvent event) {
-		EventBusRegistrator.registerModLoadingEventBuses();
-		EventBusRegistrator.registerWireframeEventBuses();
-	}
-	
-	
 }
