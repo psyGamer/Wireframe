cc3583663761fe5054e57e3572b4fc4c9131c229
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-09-13 19:27:15 +0200

    Removed core package

--- src/main/java/dev/psygamer/wireframe/Wireframe.java
+++ src/main/java/dev/psygamer/wireframe/Wireframe.java
@@ -1,5 +1,6 @@
 package dev.psygamer.wireframe;
 
+import com.google.common.collect.ImmutableList;
 import dev.psygamer.wireframe.event.EventBus;
 import dev.psygamer.wireframe.event.EventBusRegistrator;
 import dev.psygamer.wireframe.event.api.IEventBus;
@@ -7,11 +8,29 @@ import dev.psygamer.wireframe.event.api.IEventBus;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Wireframe {
 	
 	public static final String MODID = "wireframe";
 	public static final Logger LOGGER = LogManager.getLogger("Wireframe");
 	public static final IEventBus EVENT_BUS = new EventBus();
+	
+	private static final List<Mod> mods = new ArrayList<>();
+	private static Internal internal;
+	
+	public static void register(final Mod mod) {
+		mods.add(mod);
+	}
+	
+	public static ImmutableList<Mod> getMods() {
+		return ImmutableList.copyOf(mods);
+	}
+	
+	public static Internal getInternal() {
+		return internal;
+	}
 	
 	public static class Mod {
 		private final String modID, modName, modVersion;
@@ -26,6 +45,8 @@ public class Wireframe {
 	@net.minecraftforge.fml.common.Mod(Wireframe.MODID)
 	public static final class Internal {
 		public Internal() {
+			Wireframe.internal = this;
+			
 			final net.minecraftforge.eventbus.api.IEventBus modEventBus =
 					net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext.get().getModEventBus();
 			
