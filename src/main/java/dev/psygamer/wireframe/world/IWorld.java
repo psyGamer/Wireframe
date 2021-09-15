458013b0d13d4ee1a50d5c1ce262f2c6a220f5d9
psyGamer <psygamer21@gmail.com>
2021-09-15 20:12:18 +0200

    Added world abstraction interface

---
+++ src/main/java/dev/psygamer/wireframe/world/IWorld.java
@@ -0,0 +1,44 @@
+package dev.psygamer.wireframe.world;
+
+import dev.psygamer.wireframe.block.BlockFoundation;
+import dev.psygamer.wireframe.block.state.BlockState;
+import dev.psygamer.wireframe.util.BlockPosition;
+
+public interface IWorld {
+	
+	boolean isClientSide();
+	
+	boolean isServerSide();
+	
+	BlockFoundation getBlock(final BlockPosition position);
+	
+	void setBlock(final BlockFoundation block, final BlockPosition position);
+	
+	BlockState getBlockState(final BlockPosition position);
+	
+	void setBlockState(final BlockState blockState, final BlockPosition position);
+	
+	boolean isReplaceable(final BlockPosition position);
+	
+	void notifyNeighbours(final BlockPosition position);
+	
+	void notifyNeighbours(final BlockPosition position, final boolean updateObservers);
+	
+	void breakBlock(final BlockPosition position);
+	
+	void breakBlock(final BlockPosition position, final boolean dropItems);
+	
+	boolean isAir(final BlockPosition position);
+	
+	boolean isBlock(final BlockFoundation block, final BlockPosition position);
+	
+	boolean isLoaded(final BlockPosition position);
+	
+	float getBlockLightLevel(final BlockPosition blockPosition);
+	
+	float getSkyLightLevel(final BlockPosition blockPosition);
+	
+	long getTime();
+	
+	long getTicks();
+}
