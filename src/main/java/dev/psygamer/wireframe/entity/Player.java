62c2278a0b0b3883cd2ed5cf877c1a800ece75b9
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-09-16 17:15:55 +0200

    Added living entity wrapper interface

---
+++ src/main/java/dev/psygamer/wireframe/entity/Player.java
@@ -0,0 +1,26 @@
+package dev.psygamer.wireframe.entity;
+
+import dev.psygamer.wireframe.item.ItemStack;
+import dev.psygamer.wireframe.item.util.Hand;
+import dev.psygamer.wireframe.util.math.vector.Vector3d;
+
+public interface Player extends LivingEntity {
+	
+	int getHealthLevel();
+	
+	void setHealthLevel(final int healthLevel);
+	
+	int getFoodLevel();
+	
+	void setFoodLevel(int foodLevel);
+	
+	ItemStack getHeldItem(Hand hand);
+	
+	void setHeldItem(ItemStack item, Hand hand);
+	
+	Vector3d getMovementVector();
+	
+	boolean isCrouching();
+	
+	boolean isCreative();
+}
