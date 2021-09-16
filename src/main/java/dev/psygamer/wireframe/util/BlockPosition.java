b71314d415243072552e9c54a0418a274633ce5a
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-09-16 16:40:00 +0200

    BlockPosition now just extends Vector3i

--- src/main/java/dev/psygamer/wireframe/util/BlockPosition.java
+++ src/main/java/dev/psygamer/wireframe/util/BlockPosition.java
@@ -1,32 +1,18 @@
 package dev.psygamer.wireframe.util;
 
-public class BlockPosition {
-	
-	final int x, y, z;
+import dev.psygamer.wireframe.util.math.vector.Vector3i;
+
+public class BlockPosition extends Vector3i {
 	
 	public BlockPosition(final int x, final int y, final int z) {
-		this.x = x;
-		this.y = y;
-		this.z = z;
+		super(x, y, z);
 	}
 	
 	public static BlockPosition get(final net.minecraft.util.math.BlockPos internalPosition) {
 		return new BlockPosition(internalPosition.getX(), internalPosition.getY(), internalPosition.getZ());
 	}
 	
-	public int getX() {
-		return this.x;
-	}
-	
-	public int getY() {
-		return this.y;
-	}
-	
-	public int getZ() {
-		return this.z;
-	}
-	
 	public net.minecraft.util.math.BlockPos toInternal() {
-		return new net.minecraft.util.math.BlockPos(this.x, this.y, this.z);
+		return new net.minecraft.util.math.BlockPos(this.getX(), this.getY(), this.getZ());
 	}
 }
