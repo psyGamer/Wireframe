2f2a8941f971b368f87163d306bc73e346b09f11
psyGamer <psygamer21@gmail.com>
2021-09-16 16:43:42 +0200

    Added BlockRayTraceResult wrapper

---
+++ src/main/java/dev/psygamer/wireframe/util/math/HitResult.java
@@ -0,0 +1,32 @@
+package dev.psygamer.wireframe.util.math;
+
+import dev.psygamer.wireframe.util.math.vector.Vector3d;
+
+public abstract class HitResult {
+	
+	protected final Vector3d location;
+	
+	protected HitResult(final Vector3d location) {
+		this.location = location;
+	}
+	
+	public enum Type {
+		MISS, BLOCK, ENTITY
+	}
+	
+	public double distanceTo(final Vector3d other) {
+		return Math.sqrt(squaredDistanceTo(other));
+	}
+	
+	public double squaredDistanceTo(final Vector3d other) {
+		final double xDiff = this.location.getX() - other.getX();
+		final double yDiff = this.location.getY() - other.getY();
+		final double zDiff = this.location.getZ() - other.getZ();
+		
+		return xDiff * xDiff + yDiff * yDiff + zDiff * zDiff;
+	}
+	
+	public Vector3d getLocation() {
+		return this.location;
+	}
+}
