0823fa0c1d217b4db9836db9a23b0058135bfed5
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-09-18 09:33:01 +0200

    Added unit vector to Direction enum

--- src/main/java/dev/psygamer/wireframe/util/Direction.java
+++ src/main/java/dev/psygamer/wireframe/util/Direction.java
@@ -1,17 +1,23 @@
 package dev.psygamer.wireframe.util;
 
+import dev.psygamer.wireframe.util.math.vector.Vector3i;
+
 public enum Direction {
-	NORTH(net.minecraft.util.Direction.NORTH),
-	SOUTH(net.minecraft.util.Direction.SOUTH),
-	EAST(net.minecraft.util.Direction.EAST),
-	WEST(net.minecraft.util.Direction.WEST),
-	UP(net.minecraft.util.Direction.UP),
-	DOWN(net.minecraft.util.Direction.DOWN);
+	NORTH	(new Vector3i(0, 0, -1), net.minecraft.util.Direction.NORTH),
+	SOUTH	(new Vector3i(0, 0, +1), net.minecraft.util.Direction.SOUTH),
+	EAST	(new Vector3i(+1, 0, 0), net.minecraft.util.Direction.EAST),
+	WEST	(new Vector3i(-1, 0, 0), net.minecraft.util.Direction.WEST),
+	UP		(new Vector3i(0, +1, 0), net.minecraft.util.Direction.UP),
+	DOWN	(new Vector3i(0, -1, 0), net.minecraft.util.Direction.DOWN);
 	
 	private final net.minecraft.util.Direction internal;
 	
-	Direction(final net.minecraft.util.Direction internal) {
+	private final Vector3i unitVector;
+	
+	Direction(final Vector3i unitVector, final net.minecraft.util.Direction internal) {
 		this.internal = internal;
+		
+		this.unitVector = unitVector;
 	}
 	
 	public static Direction get(final net.minecraft.util.Direction internalDirection) {
@@ -30,6 +36,10 @@ public enum Direction {
 			case DOWN:
 				return DOWN;
 		}
+	}
+	
+	public Vector3i getUnitVector() {
+		return this.unitVector;
 	}
 	
 	public net.minecraft.util.Direction getInternal() {
