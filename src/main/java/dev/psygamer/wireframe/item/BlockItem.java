b417653ee648b73781e2386ef8ef848281c818f5
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-09-18 10:24:29 +0200

    Added a BlockItem

---
+++ src/main/java/dev/psygamer/wireframe/item/BlockItem.java
@@ -0,0 +1,68 @@
+package dev.psygamer.wireframe.item;
+
+import dev.psygamer.wireframe.block.Block;
+import dev.psygamer.wireframe.block.state.BlockState;
+import dev.psygamer.wireframe.block.state.property.BlockProperty;
+import dev.psygamer.wireframe.entity.Player;
+import dev.psygamer.wireframe.item.util.ClickResult;
+import dev.psygamer.wireframe.item.util.Hand;
+import dev.psygamer.wireframe.util.BlockPosition;
+import dev.psygamer.wireframe.util.Identifier;
+import dev.psygamer.wireframe.util.TagCompound;
+import dev.psygamer.wireframe.util.math.BlockHitResult;
+import dev.psygamer.wireframe.world.World;
+
+public class BlockItem extends Item {
+	
+	protected final Block block;
+	
+	public BlockItem(final Identifier identifier, final ItemAttributes attributes, final Block block) {
+		super(identifier, attributes);
+		
+		this.block = block;
+	}
+	
+	@Override
+	public ClickResult onItemUsedOnBlock(final ItemStack usedItemStack, final World world, final Player player, final Hand hand, final BlockHitResult hitResult) {
+		if (!world.isReplaceable(hitResult.getBlockPosition()))
+			return ClickResult.REJECTED;
+		
+		final BlockPosition hitPosition = hitResult.getBlockPosition();
+		final BlockPosition targetPosition = world.isReplaceable(hitPosition) ? hitPosition : hitPosition.offset(hitResult.getDirection());
+		
+		final BlockState placementState = this.block.getPlacementState(usedItemStack, world, player, hand, hitResult);
+		
+		if (!world.setBlockState(placementState, targetPosition))
+			return ClickResult.REJECTED;
+		
+		applyBlockStateTags(usedItemStack, world, world.getBlockState(targetPosition), targetPosition);
+		
+		this.block.onBlockPlaced(world.getBlockState(targetPosition), world.getBlockState(targetPosition), targetPosition, world);
+		this.block.onBlockPlacedByPlayer(world.getBlockState(targetPosition), world.getBlockState(targetPosition), targetPosition, world, player);
+		
+		if (player != null && !player.isCreative()) {
+			usedItemStack.shrink(1);
+		}
+		
+		return ClickResult.ACCEPTED;
+	}
+	
+	private void applyBlockStateTags(final ItemStack itemStack, final World world, final BlockState blockState, final BlockPosition position) {
+		final TagCompound itemTag = itemStack.getTag();
+		
+		if (itemTag == null)
+			return;
+		
+		final TagCompound blockStateTag = itemTag.getCompound("BlockStateTag");
+		
+		for (final String key : blockStateTag.getKeys()) {
+			final BlockProperty<?> property = blockState.getProperty(key);
+			
+			if (property != null) {
+				blockState.setObjectProperty(property, property.getValue(key));
+			}
+		}
+		
+		world.setBlockState(blockState,position);
+	}
+}
