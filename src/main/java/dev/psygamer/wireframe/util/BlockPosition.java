d701c3fc58890a4a62724a6beea11ca6c24bced1
psyGamer <psygamer21@gmail.com>
2021-09-18 09:35:01 +0200

    Added offset util in BlockPosition

--- src/main/java/dev/psygamer/wireframe/util/BlockPosition.java
+++ src/main/java/dev/psygamer/wireframe/util/BlockPosition.java
@@ -12,6 +12,26 @@ public class BlockPosition extends Vector3i {
 		return new BlockPosition(internalPosition.getX(), internalPosition.getY(), internalPosition.getZ());
 	}
 	
+	public BlockPosition offset(final Direction direction) {
+		return offset(1, direction);
+	}
+	
+	public BlockPosition offset(final int distance, final Direction direction) {
+		return offset(
+				direction.getUnitVector().getX() * distance,
+				direction.getUnitVector().getY() * distance,
+				direction.getUnitVector().getZ() * distance
+		);
+	}
+	
+	public BlockPosition offset(final int xOffset, final int yOffset, final int zOffset) {
+		return new BlockPosition(
+				getX() + xOffset,
+				getY() + yOffset,
+				getZ() + zOffset
+		);
+	}
+	
 	public net.minecraft.util.math.BlockPos toInternal() {
 		return new net.minecraft.util.math.BlockPos(this.getX(), this.getY(), this.getZ());
 	}
