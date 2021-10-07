cd033b77f7dfc23e91aa8e7cd1623a343399dc5f
psyGamer <psygamer21@gmail.com>
2021-10-07 16:35:30 +0200

    Default properties are now derived from the material

--- src/main/java/dev/psygamer/wireframe/block/BlockAttributes.java
+++ src/main/java/dev/psygamer/wireframe/block/BlockAttributes.java
@@ -1,12 +1,12 @@
 package dev.psygamer.wireframe.block;
 
 import dev.psygamer.wireframe.block.attributes.HarvestLevel;
+import dev.psygamer.wireframe.block.attributes.Material;
 import dev.psygamer.wireframe.internal.block.InternalBlockAttributes;
 import dev.psygamer.wireframe.util.helper.ICloneable;
 
 import net.minecraft.item.ItemGroup;
 import net.minecraft.block.SoundType;
-import net.minecraft.block.material.Material;
 
 import net.minecraftforge.common.ToolType;
 
@@ -32,6 +32,7 @@ public class BlockAttributes implements ICloneable<BlockAttributes> {
 	protected ToolType correctTool;
 	protected int harvestLevel;
 	
+	protected boolean toolRequired;
 	protected boolean fullBlock;
 	
 	/**
@@ -65,6 +66,8 @@ public class BlockAttributes implements ICloneable<BlockAttributes> {
 		this.group = group;
 		
 		this.internal = new InternalBlockAttributes(this);
+		
+		setupFromMaterial(material);
 	}
 	
 	public BlockAttributes material(final Material material) {
@@ -97,7 +100,7 @@ public class BlockAttributes implements ICloneable<BlockAttributes> {
 		return this;
 	}
 	
-	public BlockAttributes requiredTool(final ToolType tool) {
+	public BlockAttributes correctTool(final ToolType tool) {
 		this.correctTool = tool;
 		
 		return this;
@@ -111,6 +114,12 @@ public class BlockAttributes implements ICloneable<BlockAttributes> {
 	
 	public BlockAttributes harvestLevel(final HarvestLevel harvestLevel) {
 		this.harvestLevel = harvestLevel.getHarvestLevel();
+		
+		return this;
+	}
+	
+	public BlockAttributes toolRequired(final boolean toolRequired) {
+		this.toolRequired = toolRequired;
 		
 		return this;
 	}
@@ -149,6 +158,10 @@ public class BlockAttributes implements ICloneable<BlockAttributes> {
 		return this.harvestLevel;
 	}
 	
+	public boolean isToolRequired() {
+		return this.toolRequired;
+	}
+	
 	public boolean isFullBlock() {
 		return this.fullBlock;
 	}
@@ -164,9 +177,17 @@ public class BlockAttributes implements ICloneable<BlockAttributes> {
 				.group(this.group)
 				.hardness(this.hardness)
 				.blastResistance(this.blastResistance)
-				.requiredTool(this.correctTool)
+				.correctTool(this.correctTool)
 				.harvestLevel(this.harvestLevel)
+				.toolRequired(this.toolRequired)
 				.fullBlock(this.fullBlock);
+	}
+	
+	protected void setupFromMaterial(final Material material) {
+		hardness(material.getHardness());
+		blastResistance(material.getBlastResistance());
+		
+		toolRequired(material.getHardness() > 0);
 	}
 	
 	//	TODO BOUNDING BOX
