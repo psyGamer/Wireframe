67ec44c2ce7f22cec1cfbde0fa8800b470308a01
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-10-07 15:20:23 +0200

    Cleaned up Material enum

--- src/main/java/dev/psygamer/wireframe/block/attributes/Material.java
+++ src/main/java/dev/psygamer/wireframe/block/attributes/Material.java
@@ -2,80 +2,40 @@ package dev.psygamer.wireframe.block.attributes;
 
 public enum Material {
 		
-		/* Air */
-		
-		AIR,
-		STRUCTURAL_AIR,
+	AIR,
+
+	/* Natural Materials */
+
+	STONE,
+	GRASS,
+	DIRT,
+	SAND,
+	CLAY,
+
+	WOOD, FIREPROOF_WOOD,
+
+	SNOW, ICE,
+
+	WATER, LAVA, FIRE,
+
+	/* Vegetation */
+
+	PLANT, REPLACEABLE_PLANT,
+	WATER_PLANT, REPLACEABLE_WATER_PLANT,
+	FIREPROOF_PLANT, REPLACEABLE_FIREPROOF_PLANT,
+
+	VEGETABLE, LEAVES,
+
+	/* Decoration */
+
+	GLASS,
+	METAL,
+	CLOTH,
+	COBWEB,
 	
-		/* Natural Materials */
-	
-		STONE,
-		GRASS,
-		DIRT,
-		SAND,
-		CLAY,
-	
-		WOOD,
-		NETHER_WOOD,
-	
-		SNOW,
-		TOP_SNOW,
-		ICE,
-		ICE_SOLID,
-	
-		WATER,
-		LAVA,
-		FIRE,
-	
-		/* Vegetation */
-	
-		PLANT,
-		REPLACEABLE_PLANT,
-		WATER_PLANT,
-		REPLACEABLE_WATER_PLANT,
-		FIREPROOF_PLANT,
-		REPLACEABLE_FIREPROOF_PLANT,
-	
-		VEGETABLE,
-	
-		BAMBOO,
-		BAMBOO_SAPLING,
-	
-		CACTUS,
-	
-		CORAL,
-	
-		LEAVES,
-	
-		/* Decoration */
-	
-		DECORATION,
-	
-		GLASS,
-		BUILDABLE_GLASS,
-	
-		METAL,
-		HEAVY_METAL,
-	
-		SPONGE,
-		SHULKER_SHELL,
-	
-		WOOL,
-		CLOTH_DECORATION,
-	
-		WEB,
-		
-		/* Miscellaneous */
-	
-		PORTAL,
-	
-		BARRIER,
-		PISTON,
-	
-		EXPLOSIVE,
-	
-		BUBBLE_COLUMN,
-	
-		EGG,
-		CAKE
+	/* Miscellaneous */
+
+	UNBREAKABLE,
+	REPLACEABLE,
+	NO_COLLISION
 }
