3b3e6d684ab3c1a822b90ce6bd21c3e708eda542
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-10-07 15:57:17 +0200

    Added common attributes to Materials

--- src/main/java/dev/psygamer/wireframe/block/attributes/Material.java
+++ src/main/java/dev/psygamer/wireframe/block/attributes/Material.java
@@ -2,40 +2,80 @@ package dev.psygamer.wireframe.block.attributes;
 
 public enum Material {
 		
-	AIR,
+	AIR(0.0f, true, false),
 
 	/* Natural Materials */
 
-	STONE,
-	GRASS,
-	DIRT,
-	SAND,
-	CLAY,
+	STONE(1.5f, 6.0f),
+	GRASS(0.6f),
+	DIRT(0.5f),
+	SAND(0.5f),
+	CLAY(0.6f),
 
-	WOOD, FIREPROOF_WOOD,
+	WOOD(2.0f, 3.0f, false, true),
+	FIREPROOF_WOOD(2.0f, 3.0f, false, false),
 
-	SNOW, ICE,
-
-	WATER, LAVA, FIRE,
+	SNOW(0.2f), ICE(0.5f),
 
 	/* Vegetation */
 
-	PLANT, REPLACEABLE_PLANT,
-	WATER_PLANT, REPLACEABLE_WATER_PLANT,
-	FIREPROOF_PLANT, REPLACEABLE_FIREPROOF_PLANT,
+	PLANT(0.0f, false, true),
+	REPLACEABLE_PLANT(0.0f, true, true),
+	FIREPROOF_PLANT(0.0f, false, false),
+	REPLACEABLE_FIREPROOF_PLANT(0.0f, true, false),
 
-	VEGETABLE, LEAVES,
+	VEGETABLE(0.0f, false, false),
+	LEAVES(0.2f, false, true),
 
 	/* Decoration */
 
-	GLASS,
-	METAL,
-	CLOTH,
-	COBWEB,
+	GLASS(0.3f),
+	METAL(5.0f, 6.0f),
+	CLOTH(0.8f, false, true),
 	
 	/* Miscellaneous */
 
-	UNBREAKABLE,
-	REPLACEABLE,
-	NO_COLLISION
+	UNBREAKABLE(-1.0f, 3600000.0f);
+	
+	private final float hardness;
+	private final float blastResistance;
+	
+	private final boolean replaceable;
+	private final boolean flammable;
+	
+	Material(final float strength) {
+		this(strength, strength, false, false);
+	}
+	
+	Material(final float hardness, final float blastResistance) {
+		this(hardness, blastResistance, false, false);
+	}
+	
+	Material(final float strength, final boolean replaceable, final boolean flammable) {
+		this(strength, strength, replaceable, flammable);
+	}
+	
+	Material(final float hardness, final float blastResistance, final boolean replaceable, final boolean flammable) {
+		this.hardness = hardness;
+		this.blastResistance = blastResistance;
+		
+		this.replaceable = replaceable;
+		this.flammable = flammable;
+	}
+	
+	public float getHardness() {
+		return this.hardness;
+	}
+	
+	public float getBlastResistance() {
+		return this.blastResistance;
+	}
+	
+	public boolean isReplaceable() {
+		return this.replaceable;
+	}
+	
+	public boolean isFlammable() {
+		return this.flammable;
+	}
 }
