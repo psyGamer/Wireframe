075b72025b9d556742e739e8330edd517e023bb0
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-10-07 19:05:16 +0200

    Include internal in Material

--- src/main/java/dev/psygamer/wireframe/block/attributes/Material.java
+++ src/main/java/dev/psygamer/wireframe/block/attributes/Material.java
@@ -2,40 +2,43 @@ package dev.psygamer.wireframe.block.attributes;
 
 public enum Material {
 		
-	AIR(0.0f, true, false),
+	AIR(net.minecraft.block.material.Material.AIR, true, false, 0.0f),
 
 	/* Natural Materials */
 
-	STONE(1.5f, 6.0f),
-	GRASS(0.6f),
-	DIRT(0.5f),
-	SAND(0.5f),
-	CLAY(0.6f),
+	STONE(net.minecraft.block.material.Material.STONE, 6.0f, 1.5f),
+	GRASS(net.minecraft.block.material.Material.GRASS, 0.6f),
+	DIRT(net.minecraft.block.material.Material.DIRT, 0.5f),
+	SAND(net.minecraft.block.material.Material.SAND, 0.5f),
+	CLAY(net.minecraft.block.material.Material.CLAY, 0.6f),
 
-	WOOD(2.0f, 3.0f, false, true),
-	FIREPROOF_WOOD(2.0f, 3.0f, false, false),
+	WOOD(net.minecraft.block.material.Material.WOOD, 2.0f, 3.0f, false, true),
+	FIREPROOF_WOOD(net.minecraft.block.material.Material.NETHER_WOOD, 2.0f, 3.0f, false, false),
 
-	SNOW(0.2f), ICE(0.5f),
+	SNOW(net.minecraft.block.material.Material.SNOW, 0.2f),
+	ICE(net.minecraft.block.material.Material.ICE, 0.5f),
 
 	/* Vegetation */
 
-	PLANT(0.0f, false, true),
-	REPLACEABLE_PLANT(0.0f, true, true),
-	FIREPROOF_PLANT(0.0f, false, false),
-	REPLACEABLE_FIREPROOF_PLANT(0.0f, true, false),
+	PLANT(net.minecraft.block.material.Material.PLANT, false, true, 0.0f),
+	REPLACEABLE_PLANT(net.minecraft.block.material.Material.REPLACEABLE_PLANT, true, true, 0.0f),
+	FIREPROOF_PLANT(net.minecraft.block.material.Material.REPLACEABLE_FIREPROOF_PLANT, false, false, 0.0f),
+	REPLACEABLE_FIREPROOF_PLANT(net.minecraft.block.material.Material.REPLACEABLE_FIREPROOF_PLANT, true, false, 0.0f),
 
-	VEGETABLE(0.0f, false, false),
-	LEAVES(0.2f, false, true),
+	VEGETABLE(net.minecraft.block.material.Material.VEGETABLE, false, false, 0.0f),
+	LEAVES(net.minecraft.block.material.Material.LEAVES, false, true, 0.2f),
 
 	/* Decoration */
 
-	GLASS(0.3f),
-	METAL(5.0f, 6.0f),
-	CLOTH(0.8f, false, true),
+	GLASS(net.minecraft.block.material.Material.GLASS, 0.3f),
+	METAL(net.minecraft.block.material.Material.METAL, 6.0f, 5.0f),
+	CLOTH(net.minecraft.block.material.Material.WOOL, false, true, 0.8f),
 	
 	/* Miscellaneous */
 
-	UNBREAKABLE(-1.0f, 3600000.0f);
+	UNBREAKABLE(net.minecraft.block.material.Material.STONE, 3600000.0f, -1.0f);
+	
+	private final net.minecraft.block.material.Material internal;
 	
 	private final float hardness;
 	private final float blastResistance;
@@ -43,19 +46,21 @@ public enum Material {
 	private final boolean replaceable;
 	private final boolean flammable;
 	
-	Material(final float strength) {
-		this(strength, strength, false, false);
+	Material(final net.minecraft.block.material.Material internal, final float strength) {
+		this(internal, strength, strength, false, false);
 	}
 	
-	Material(final float hardness, final float blastResistance) {
-		this(hardness, blastResistance, false, false);
+	Material(final net.minecraft.block.material.Material internal, final float blastResistance, final float hardness) {
+		this(internal, hardness, blastResistance, false, false);
 	}
 	
-	Material(final float strength, final boolean replaceable, final boolean flammable) {
-		this(strength, strength, replaceable, flammable);
+	Material(final net.minecraft.block.material.Material internal, final boolean replaceable, final boolean flammable, final float strength) {
+		this(internal, strength, strength, replaceable, flammable);
 	}
 	
-	Material(final float hardness, final float blastResistance, final boolean replaceable, final boolean flammable) {
+	Material(final net.minecraft.block.material.Material internal, final float hardness, final float blastResistance, final boolean replaceable, final boolean flammable) {
+		this.internal = internal;
+		
 		this.hardness = hardness;
 		this.blastResistance = blastResistance;
 		
@@ -77,5 +82,9 @@ public enum Material {
 	
 	public boolean isFlammable() {
 		return this.flammable;
+	}
+	
+	public net.minecraft.block.material.Material getInternal() {
+		return this.internal;
 	}
 }
