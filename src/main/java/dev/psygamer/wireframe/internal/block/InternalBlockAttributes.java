df21c22c1f1ea652d570cc694dc0d2563635cef7
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-10-07 14:53:23 +0200

    Fixed BlockProperties creation

--- src/main/java/dev/psygamer/wireframe/internal/block/InternalBlockAttributes.java
+++ src/main/java/dev/psygamer/wireframe/internal/block/InternalBlockAttributes.java
@@ -15,33 +15,28 @@ public class InternalBlockAttributes {
 	public AbstractBlock.Properties createProperties() {
 		final AbstractBlock.Properties properties = AbstractBlock.Properties.of(this.attributes.getMaterial());
 		
-		if (this.attributes.getHardness() > 0 && this.attributes.getBlastResistance() > 0) {
+		if (this.attributes.getHardness() >= 0 && this.attributes.getBlastResistance() >= 0)
 			properties.strength(this.attributes.getHardness(), this.attributes.getBlastResistance());
-		} else if (this.attributes.getHardness() > 0) {
+		else if (this.attributes.getHardness() >= 0)
 			properties.strength(this.attributes.getHardness());
-		}
-		
-		if (this.attributes.getCorrectTool() == null) {
+		else
 			properties.instabreak();
-		} else {
+		
+		if (this.attributes.getCorrectTool() != null)
 			properties.harvestTool(this.attributes.getCorrectTool());
-		}
 		
 		properties.harvestLevel(this.attributes.getHarvestLevel());
 		
-		if (this.attributes.getHarvestLevel() == HarvestLevel.NONE.getHarvestLevel()) {
+		if (this.attributes.getHarvestLevel() != HarvestLevel.NONE.getHarvestLevel())
 			properties.requiresCorrectToolForDrops();
-		}
 		
-		if (this.attributes.getSound() != null) {
+		if (this.attributes.getSound() != null)
 			properties.sound(this.attributes.getSound());
-		}
 		
 		if (this.attributes.isFullBlock()) {
 			properties.isValidSpawn(AbstractBlock.AbstractBlockState::isValidSpawn);
 			properties.isSuffocating(AbstractBlock.AbstractBlockState::isSuffocating);
 			properties.isViewBlocking(AbstractBlock.AbstractBlockState::isViewBlocking);
-			properties.noCollission();
 		}
 		
 		return properties;
