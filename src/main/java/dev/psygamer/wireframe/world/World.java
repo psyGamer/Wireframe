f0fe2ac32bc35f21ccb96a3f42a098453944471c
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-10-09 17:22:57 +0200

    Changed World$UpdateFlag to an enum

--- src/main/java/dev/psygamer/wireframe/world/World.java
+++ src/main/java/dev/psygamer/wireframe/world/World.java
@@ -7,6 +7,64 @@ import dev.psygamer.wireframe.util.BlockPosition;
 
 public interface World extends BlockReader {
 	
+	enum UpdateFlag {
+		
+		/**
+		 * Causes neighbor updates to be sent to all surrounding blocks.
+		 */
+		NOTIFY_NEIGHBORS(1 << 0),
+		
+		/** Updates path-finding */
+		BLOCK_UPDATE(1 << 1),
+		
+		/** Stops the blocks from being marked for a render update */
+		NO_RERENDER(1 << 2),
+		
+		/**
+		 * Makes the block be re-rendered immediately, on the main thread.
+		 * If NO_RERENDER is set, then this will be ignored
+		 */
+		RERENDER_MAIN_THREAD(1 << 3),
+		
+		/**
+		 * Causes neighbor updates to be sent to all surrounding blocks (including
+		 * diagonals).
+		 */
+		UPDATE_NEIGHBORS(1 << 4),
+		
+		/**
+		 * Prevents neighbor changes from spawning item drops, used by
+		 */
+		NO_NEIGHBOR_DROPS(1 << 5),
+		
+		/**
+		 * Tell the block being changed that it was moved, rather than removed/replaced,
+		 */
+		IS_MOVING(1 << 6),
+		
+		/**
+		 * Causes neighbor updates to be sent to all surrounding blocks
+		 * and updates path-finding.
+		 */
+		DEFAULT(NOTIFY_NEIGHBORS.internalFlag | BLOCK_UPDATE.internalFlag),
+		
+		/**
+		 * Causes neighbor updates to be sent to all surrounding blocks, updates path-finding
+		 * and Makes the block be re-rendered immediately, on the main thread.
+		 */
+		DEFAULT_AND_RERENDER(DEFAULT.internalFlag | RERENDER_MAIN_THREAD.internalFlag);
+		
+		private final int internalFlag;
+		
+		UpdateFlag(final int internalFlag) {
+			this.internalFlag = internalFlag;
+		}
+		
+		public int getInternalFlag() {
+			return internalFlag;
+		}
+	}
+	
 	static World get(final net.minecraft.world.World internalWorld) {
 		if (internalWorld == null)
 			return null;
@@ -20,11 +78,11 @@ public interface World extends BlockReader {
 	
 	boolean setBlock(final Block block, final BlockPosition position);
 	
-	boolean setBlock(final Block block, final BlockPosition position, final int updateFlag);
+	boolean setBlock(final Block block, final BlockPosition position, final UpdateFlag updateFlag);
 	
 	boolean setBlockState(final BlockState blockState, final BlockPosition position);
 	
-	boolean setBlockState(final BlockState blockState, final BlockPosition position, final int updateFlag);
+	boolean setBlockState(final BlockState blockState, final BlockPosition position, final UpdateFlag updateFlag);
 	
 	boolean isReplaceable(final BlockPosition position);
 	
@@ -49,52 +107,4 @@ public interface World extends BlockReader {
 	long getTime();
 	
 	long getTicks();
-	
-	final class UpdateFlags {
-		
-		/**
-		 * Causes neighbor updates to be sent to all surrounding blocks.
-		 */
-		public static final int NOTIFY_NEIGHBORS = (1 << 0);
-		
-		/** Updates path-finding */
-		public static final int BLOCK_UPDATE = (1 << 1);
-		
-		/** Stops the blocks from being marked for a render update */
-		public static final int NO_RERENDER = (1 << 2);
-		
-		/**
-		 * Makes the block be re-rendered immediately, on the main thread.
-		 * If NO_RERENDER is set, then this will be ignored
-		 */
-		public static final int RERENDER_MAIN_THREAD = (1 << 3);
-		
-		/**
-		 * Causes neighbor updates to be sent to all surrounding blocks (including
-		 * diagonals).
-		 */
-		public static final int UPDATE_NEIGHBORS = (1 << 4);
-		
-		/**
-		 * Prevents neighbor changes from spawning item drops, used by
-		 */
-		public static final int NO_NEIGHBOR_DROPS = (1 << 5);
-		
-		/**
-		 * Tell the block being changed that it was moved, rather than removed/replaced,
-		 */
-		public static final int IS_MOVING = (1 << 6);
-		
-		/**
-		 * Causes neighbor updates to be sent to all surrounding blocks
-		 * and updates path-finding.
-		 */
-		public static final int DEFAULT = NOTIFY_NEIGHBORS | BLOCK_UPDATE;
-		
-		/**
-		 * Causes neighbor updates to be sent to all surrounding blocks, updates path-finding
-		 * and Makes the block be re-rendered immediately, on the main thread.
-		 */
-		public static final int DEFAULT_AND_RERENDER = DEFAULT | RERENDER_MAIN_THREAD;
-	}
 }
