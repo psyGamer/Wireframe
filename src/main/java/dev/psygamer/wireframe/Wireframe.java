bcaf8cb32264aae3c9f37e5959ce67cdad206262
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-10-23 10:51:43 +0200

    Added documentation to class `Wireframe`

--- src/main/java/dev/psygamer/wireframe/Wireframe.java
+++ src/main/java/dev/psygamer/wireframe/Wireframe.java
@@ -1,38 +1,48 @@
 package dev.psygamer.wireframe;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Lists;
 import dev.psygamer.wireframe.event.EventBus;
 import dev.psygamer.wireframe.event.EventBusRegistrator;
 import dev.psygamer.wireframe.event.api.IEventBus;
 import dev.psygamer.wireframe.test.item_display.ItemDisplayBlock;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 
+/** The main class of the Wireframe Framework */
 public class Wireframe {
 	
 	public static final String MODID = "wireframe";
 	public static final Logger LOGGER = LogManager.getLogger("Wireframe");
 	public static final IEventBus EVENT_BUS = new EventBus();
 	
+	/** All registered mods which have Wireframe as dependency. */
 	private static final List<Mod> mods = Lists.newArrayList(new InternalMod());
 	
+	/** Internal, do not use! */
 	private static Internal internal;
 	
 	public static void register(final Mod mod) {
 		mods.add(mod);
 	}
 	
+	/** Returns a list of all child mods. */
 	public static ImmutableList<Mod> getMods() {
 		return ImmutableList.copyOf(mods);
 	}
 	
+	/** Internal, do not use! */
 	public static Internal getInternal() {
 		return internal;
 	}
 	
+	/**
+	 * A class which all child mods must extend.
+	 * This contains basic information about a child mod.
+	 */
 	public static class Mod {
 		
 		private final String modID, modName, modVersion, rootPackage;
@@ -40,6 +50,7 @@ public class Wireframe {
 		
 		private final net.minecraftforge.eventbus.api.IEventBus internalModEventBus;
 		
+		/** Constructs a new child mod container */
 		protected Mod(final String modID, final String modName, final String modVersion) {
 			this.modID = modID;
 			this.modName = modName;
@@ -65,6 +76,7 @@ public class Wireframe {
 			return this.modVersion;
 		}
 		
+		/** The package-path of class which extends this class. */
 		public String getRootPackage() {
 			return this.rootPackage;
 		}
@@ -73,11 +85,13 @@ public class Wireframe {
 			return this.modEventBus;
 		}
 		
+		/** Internal, do not use! */
 		public net.minecraftforge.eventbus.api.IEventBus getInternalModEventBus() {
 			return this.internalModEventBus;
 		}
 	}
 	
+	/** Internal, do not use! */
 	@net.minecraftforge.fml.common.Mod(Wireframe.MODID)
 	public static final class Internal {
 		
@@ -99,7 +113,7 @@ public class Wireframe {
 		}
 	}
 	
-	/* A Wireframe$Mod implementation for native stuff */
+	/** Internal, do not use! */
 	private static final class InternalMod extends Mod {
 		
 		private InternalMod() {
