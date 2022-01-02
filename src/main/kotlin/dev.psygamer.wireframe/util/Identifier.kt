85f9d5e64f7af64e079cfc36d98e69f9461d4fe2
psyGamer <psygamer21@gmail.com>
2022-01-02 20:51:38 +0100

    Migrated `Identifier` to Kotlin

--- src/main/kotlin/dev.psygamer.wireframe/util/Identifier.kt
+++ src/main/kotlin/dev.psygamer.wireframe/util/Identifier.kt
@@ -1,13 +1,17 @@
 package dev.psygamer.wireframe.util
 
-val net.minecraft.util.ResourceLocation.wfWrapped: Identifier
-	get() {
-		return Identifier(this.namespace, this.path)
-	}
-
 data class Identifier(val namespace: String = "minecraft", val path: String) {
 	
 	override fun toString(): String {
 		return "$namespace:$path"
 	}
 }
+
+val net.minecraft.util.ResourceLocation.wfWrapped: Identifier
+	get() {
+		return Identifier(this.namespace, this.path)
+	}
+val Identifier.mcNative: net.minecraft.util.ResourceLocation
+	get() {
+		return net.minecraft.util.ResourceLocation(this.namespace, this.path)
+	}
