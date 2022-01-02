16084aa575b7d5f883fd652cda517ab3d24fefed
psyGamer <psygamer21@gmail.com>
2022-01-02 22:19:43 +0100

    Added many utility methods to `Vector`s

--- src/main/kotlin/dev/psygamer/wireframe/util/math/vector/Vector2f.kt
+++ src/main/kotlin/dev/psygamer/wireframe/util/math/vector/Vector2f.kt
@@ -1,6 +1,66 @@
 package dev.psygamer.wireframe.util.math.vector
 
-data class Vector2f(val x: Float, val y: Float)
+import kotlin.math.acos
+import kotlin.math.atan2
+import kotlin.math.sqrt
+
+data class Vector2f(val x: Float, val y: Float) {
+	operator fun plus(other: Vector2f): Vector2f {
+		return Vector2f(this.x + other.x, this.y + other.y)
+	}
+	
+	operator fun minus(other: Vector2f): Vector2f {
+		return Vector2f(this.x - other.x, this.y - other.y)
+	}
+	
+	operator fun times(other: Vector2f): Vector2f {
+		return Vector2f(this.x * other.x, this.y * other.y)
+	}
+	
+	operator fun times(scalar: Float): Vector2f {
+		return Vector2f((this.x * scalar), (this.y * scalar))
+	}
+	
+	operator fun div(other: Vector2f): Vector2f {
+		return Vector2f(this.x / other.x, y / other.y)
+	}
+	
+	operator fun div(scalar: Float): Vector2f {
+		return Vector2f((this.x / scalar), (this.y / scalar))
+	}
+	
+	infix fun cross(other: Vector2f): Vector2f {
+		return Vector2f(this.x * other.y, this.y * other.x)
+	}
+	
+	infix fun dot(other: Vector2f): Float {
+		return (this.x * other.x + this.y * other.y)
+	}
+	
+	val magnitude: Float
+		get() = sqrt(this.x * this.x + this.y * this.y)
+	
+	val magnitudeSquared: Float
+		get() = this.x * this.x + this.y * this.y
+	
+	fun distance(other: Vector2f): Float {
+		return (this - other).magnitude
+	}
+	
+	fun distanceSquared(other: Vector2f): Float {
+		return (this - other).magnitudeSquared
+	}
+	
+	val normalized: Vector2f
+		get() = this / this.magnitude
+	
+	val angle: Float
+		get() = atan2(this.x, this.y)
+	
+	fun angleBetween(other: Vector2f): Float {
+		return acos(this dot other / (this.magnitude * other.magnitude))
+	}
+}
 
 val net.minecraft.util.math.vector.Vector2f.wfWrapped: Vector2f
 	get() = Vector2f(this.x, this.y)
--- src/main/kotlin/dev/psygamer/wireframe/util/math/vector/Vector3d.kt
+++ src/main/kotlin/dev/psygamer/wireframe/util/math/vector/Vector3d.kt
@@ -1,6 +1,66 @@
 package dev.psygamer.wireframe.util.math.vector
 
-data class Vector3d(val x: Double, val y: Double, val z: Double)
+import kotlin.math.acos
+import kotlin.math.atan2
+import kotlin.math.sqrt
+
+data class Vector3d(val x: Double, val y: Double, val z: Double) {
+	operator fun plus(other: Vector3d): Vector3d {
+		return Vector3d(this.x + other.x, this.y + other.y, this.z + other.z)
+	}
+	
+	operator fun minus(other: Vector3d): Vector3d {
+		return Vector3d(this.x - other.x, this.y - other.y, this.z + other.z)
+	}
+	
+	operator fun times(other: Vector3d): Vector3d {
+		return Vector3d(this.x * other.x, this.y * other.y, this.z + other.z)
+	}
+	
+	operator fun times(scalar: Double): Vector3d {
+		return Vector3d((this.x * scalar), (this.y * scalar), (this.z * scalar))
+	}
+	
+	operator fun div(other: Vector3d): Vector3d {
+		return Vector3d(this.x / other.x, this.y / other.y, this.z / other.z)
+	}
+	
+	operator fun div(scalar: Double): Vector3d {
+		return Vector3d((this.x / scalar), (this.y / scalar), (this.z / scalar))
+	}
+	
+	infix fun cross(other: Vector3d): Vector3d {
+		return Vector3d(this.x * other.y, this.y * other.x, this.z * other.z)
+	}
+	
+	infix fun dot(other: Vector3d): Double {
+		return (this.x * other.x + this.y * other.y + this.z * other.z)
+	}
+	
+	val magnitude: Double
+		get() = sqrt(this.x * this.x + this.y * this.y + this.z * this.z)
+	
+	val magnitudeSquared: Double
+		get() = (this.x * this.x + this.y * this.y + this.z * this.z)
+	
+	fun distance(other: Vector3d): Double {
+		return (this - other).magnitude
+	}
+	
+	fun distanceSquared(other: Vector3d): Double {
+		return (this - other).magnitudeSquared
+	}
+	
+	val normalized: Vector3d
+		get() = this / this.magnitude
+	
+	val angle: Double
+		get() = atan2(this.x, this.y)
+	
+	fun angleBetween(other: Vector3d): Double {
+		return acos(this dot other / (this.magnitude * other.magnitude))
+	}
+}
 
 val net.minecraft.util.math.vector.Vector3d.wfWrapped: Vector3d
 	get() = Vector3d(this.x, this.y, this.z)
