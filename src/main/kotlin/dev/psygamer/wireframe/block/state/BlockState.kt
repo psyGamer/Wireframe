83aa884743bf6405ca0ca2e2d4eb1addbc6aea88
psyGamer <56753271+psyGamer@users.noreply.github.com>
2022-01-03 23:05:57 +0100

    Migrated `BlockState` & `BlockStateDefinition` to Kotlin

---
+++ src/main/kotlin/dev/psygamer/wireframe/block/state/BlockState.kt
@@ -0,0 +1,23 @@
+package dev.psygamer.wireframe.block.state
+
+import dev.psygamer.wireframe.block.Block
+import dev.psygamer.wireframe.block.state.property.BlockProperty
+import dev.psygamer.wireframe.internal.block.InternalBlockState
+import java.util.*
+
+interface BlockState {
+	
+	val block: Block
+	
+	val mcNative: net.minecraft.block.BlockState
+	
+	fun <T : Comparable<T>> setValue(property: BlockProperty<T>, value: T?): BlockState
+	
+	fun <T : Comparable<T>> getValue(property: BlockProperty<T>): T?
+	fun <T : Comparable<T>> getOptionalValue(property: BlockProperty<T>): Optional<T>
+	
+	fun `is`(block: Block?): Boolean
+}
+
+internal val net.minecraft.block.BlockState.wfWrapped: BlockState
+	get() = InternalBlockState(this)
