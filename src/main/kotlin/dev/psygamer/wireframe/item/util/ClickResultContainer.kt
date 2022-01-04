bdf27daae363134db4dfa93630a5ead397a72387
psyGamer <56753271+psyGamer@users.noreply.github.com>
2022-01-04 15:05:26 +0100

    Migrated `ClickResultContainer` to Kotlin

---
+++ src/main/kotlin/dev/psygamer/wireframe/item/util/ClickResultContainer.kt
@@ -0,0 +1,22 @@
+package dev.psygamer.wireframe.item.util
+
+data class ClickResultContainer<T>(val obj: T?, val result: ClickResult) {
+	
+	companion object {
+		
+		@JvmStatic
+		fun <T> accept(obj: T?): ClickResultContainer<T> {
+			return ClickResultContainer(obj, ClickResult.ACCEPTED)
+		}
+		
+		@JvmStatic
+		fun <T> reject(obj: T?): ClickResultContainer<T> {
+			return ClickResultContainer(obj, ClickResult.REJECTED)
+		}
+		
+		@JvmStatic
+		fun <T> pass(obj: T?): ClickResultContainer<T> {
+			return ClickResultContainer(obj, ClickResult.PASS)
+		}
+	}
+}
