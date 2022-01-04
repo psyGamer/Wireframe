31123097316d9ccf5da56a00d62a6a5353107190
psyGamer <56753271+psyGamer@users.noreply.github.com>
2022-01-04 13:48:51 +0100

    Migrated `EventSubscriber`, `EventBusSubscriber`, `ModEventBusSubscriber` to Kotlin

---
+++ src/main/kotlin/dev/psygamer/wireframe/event/api/EventSubscriber.kt
@@ -0,0 +1,5 @@
+package dev.psygamer.wireframe.event.api
+
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class EventSubscriber(val priority: EventPriority)
