51001ee9367f69f57e91c90a1dd0a5729d4f7275
psyGamer <psygamer21@gmail.com>
2022-01-08 18:25:44 +0100

    `BlockEntityRegistry` now uses `MapRegistry`

--- src/main/kotlin/dev/psygamer/wireframe/api/registry/BlockEntityRegistry.kt
+++ src/main/kotlin/dev/psygamer/wireframe/api/registry/BlockEntityRegistry.kt
@@ -2,26 +2,12 @@ package dev.psygamer.wireframe.api.registry
 
 import dev.psygamer.wireframe.api.block.entity.BlockEntity.Definition
 import dev.psygamer.wireframe.util.Identifier
-import dev.psygamer.wireframe.util.collection.FreezableHashMap
 import dev.psygamer.wireframe.util.collection.FreezableMap
 
-object BlockEntityRegistry {
-	
-	@JvmStatic
-	val definitions: Map<Identifier, Definition> = FreezableHashMap()
+object BlockEntityRegistry : MapRegistry<Identifier, Definition>() {
 	
 	@JvmStatic
 	fun register(definition: Definition) {
-		(definitions as FreezableMap)[definition.identifier] = definition
-	}
-	
-	@JvmStatic
-	fun getDefinition(identifier: Identifier): Definition? {
-		return definitions[identifier]
-	}
-	
-	@JvmStatic
-	fun freeze() {
-		(definitions as FreezableMap).freeze()
+		(elements as FreezableMap)[definition.identifier] = definition
 	}
 }
