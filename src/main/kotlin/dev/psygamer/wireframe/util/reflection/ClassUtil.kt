0b3bfd6919d1ce7902c674ec801536c816c4b4fa
psyGamer <56753271+psyGamer@users.noreply.github.com>
2022-01-17 19:25:21 +0100

    `EventBusRegistrator` no longer initializes classes

--- src/main/kotlin/dev/psygamer/wireframe/util/reflection/ClassUtil.kt
+++ src/main/kotlin/dev/psygamer/wireframe/util/reflection/ClassUtil.kt
@@ -6,11 +6,14 @@ import javax.tools.ToolProvider
 
 object ClassUtil {
 	
-	fun getClasses(packageName: String): List<Class<*>> {
-		return getClassesWithAnnotation(packageName, null)
+	fun getClasses(packageName: String, initializeClass: Boolean = true, classLoader: ClassLoader? = null): List<Class<*>> {
+		return getClassesWithAnnotation(packageName, null, initializeClass, classLoader)
 	}
 	
-	fun getClassesWithAnnotation(packageName: String, annotationClass: Class<out Annotation>?): List<Class<*>> {
+	fun getClassesWithAnnotation(
+		packageName: String, annotationClass: Class<out Annotation>?, initializeClass: Boolean = true,
+		classLoader: ClassLoader? = null
+	): List<Class<*>> {
 		val classes: MutableList<Class<*>> = ArrayList()
 		val compiler = ToolProvider.getSystemJavaCompiler()
 		val fileManager = compiler.getStandardFileManager(null, null, null)
@@ -32,7 +35,11 @@ object ClassUtil {
 			fullName = fullName.replace("/".toRegex(), ".")
 			fullName = fullName.substring(0, fullName.length - ".class".length)
 			
-			val clazz = Class.forName(fullName)
+			val clazz =
+				if (initializeClass)
+					Class.forName(fullName)
+				else
+					Class.forName(fullName, false, classLoader)
 			
 			if (annotationClass == null || clazz.isAnnotationPresent(annotationClass))
 				classes.add(clazz)
