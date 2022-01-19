9683a31d39ca0e14659c3ab00a2acbb454530889
psyGamer <56753271+psyGamer@users.noreply.github.com>
2022-01-19 17:09:22 +0100

    Added `ScreenManager`

---
+++ src/main/kotlin/dev/psygamer/wireframe/api/client/screen/ScreenManager.kt
@@ -0,0 +1,11 @@
+package dev.psygamer.wireframe.api.client.screen
+
+import dev.psygamer.wireframe.nativeapi.client.screen.NativeScreenManager
+
+object ScreenManager {
+	
+	fun Screen.open() = openScreen(this)
+	
+	fun openScreen(screen: Screen) =
+		NativeScreenManager.openScreen(screen)
+}
