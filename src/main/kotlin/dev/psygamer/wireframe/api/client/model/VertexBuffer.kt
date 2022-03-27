23b63e9a3e2f6531b61550b52c430f596f6962eb
psyGamer <psygamer21@gmail.com>
2022-03-27 01:04:22 +0100

    Added `VertexBuffer` and `VertexBuffer$Builder`
    Supports: normals, texture coords and vertex colors

---
+++ src/main/kotlin/dev/psygamer/wireframe/api/client/model/VertexBuffer.kt
@@ -0,0 +1,71 @@
+package dev.psygamer.wireframe.api.client.model
+
+class VertexBuffer(data: FloatArray?, val size: Int, hasNormals: Boolean, hasTexCoords: Boolean, hasColors: Boolean) {
+	
+	val data = run {
+		if (data != null)
+			return@run data
+		return@run FloatArray(size * this.stride)
+	}
+	
+	val stride: Int =
+		(if (hasNormals) 3 else 0) +
+		(if (hasTexCoords) 2 else 0) +
+		(if (hasColors) 3 else 0)
+	
+	val vertexOffset: Int = 0
+	val normalOffset: Int = if (hasNormals) vertexOffset + 3 else vertexOffset
+	val texCoordOffset: Int = if (hasTexCoords) normalOffset + 2 else normalOffset
+	val colorOffset: Int = if (hasColors) texCoordOffset + 3 else texCoordOffset
+	
+	class Builder {
+		
+		private val vertices = emptyList<Vertex>().toMutableList()
+		
+		fun addVertex(x: Float, y: Float, z: Float): Vertex {
+			val vertex = Vertex(x, y, z)
+			vertices.add(vertex)
+			return vertex
+		}
+		
+		fun build(): VertexBuffer {
+			var hasNormals = false
+			var hasTexCoords = false
+			var hasColors = false
+			
+			for (vertex in vertices) {
+				hasNormals = hasNormals or vertex.hasNormal
+				hasTexCoords = hasTexCoords or vertex.hasTexCoord
+				hasColors = hasColors or vertex.hasColor
+			}
+			
+			val buffer = VertexBuffer(data = null, size = this.vertices.size,
+									  hasNormals, hasTexCoords, hasColors)
+			
+			for (i in 0..this.vertices.size) {
+				val vertex = vertices[i]
+				
+				buffer.data[i * buffer.stride + buffer.vertexOffset] = vertex.x
+				buffer.data[i * buffer.stride + buffer.vertexOffset + 1] = vertex.y
+				buffer.data[i * buffer.stride + buffer.vertexOffset + 2] = vertex.z
+				
+				if (hasNormals) {
+					buffer.data[i * buffer.stride + buffer.normalOffset] = vertex.nx
+					buffer.data[i * buffer.stride + buffer.normalOffset + 1] = vertex.ny
+					buffer.data[i * buffer.stride + buffer.normalOffset + 2] = vertex.nz
+				}
+				if (hasTexCoords) {
+					buffer.data[i * buffer.stride + buffer.texCoordOffset] = vertex.u
+					buffer.data[i * buffer.stride + buffer.texCoordOffset + 1] = vertex.v
+				}
+				if (hasColors) {
+					buffer.data[i * buffer.stride + buffer.colorOffset] = vertex.r
+					buffer.data[i * buffer.stride + buffer.colorOffset + 1] = vertex.g
+					buffer.data[i * buffer.stride + buffer.colorOffset + 2] = vertex.b
+				}
+			}
+			
+			return buffer
+		}
+	}
+}
