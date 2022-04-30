0f4d37882c0652b73a9f1f79c0def20f424b213c
psyGamer <psygamer21@gmail.com>
2022-04-30 18:08:43 +0200

    Migrated `HitResult` & `BlockHitResult` to Kotlin

---
+++ src/main/kotlin/dev/psygamer/wireframe/util/math/BlockHitResult.kt
@@ -0,0 +1,12 @@
+package dev.psygamer.wireframe.util.math
+
+import dev.psygamer.wireframe.util.*
+import dev.psygamer.wireframe.util.math.vector.Vector3d
+
+class BlockHitResult(
+	location: Vector3d, val direction: Direction, val blockPosition: BlockPosition,
+	miss: Boolean = false, val isInside: Boolean,
+) : HitResult(location) {
+	
+	val type: Type = if (miss) Type.MISS else Type.BLOCK
+}
