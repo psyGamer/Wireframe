64471c67e1faa0c99ad50df5658ce87ca68f0e83
psyGamer <psygamer21@gmail.com>
2022-04-30 18:58:08 +0200

    Migrated freezable collections to Kotlin

---
+++ src/main/kotlin/dev/psygamer/wireframe/util/collection/FreezableArrayList.kt
@@ -0,0 +1,8 @@
+package dev.psygamer.wireframe.util.collection
+
+class FreezableArrayList<E> : FreezableList<E> {
+	
+	constructor() : super(ArrayList<E>())
+	constructor(initialCapacity: Int) : super(ArrayList<E>(initialCapacity))
+	constructor(elements: Collection<E>) : super(ArrayList<E>(elements))
+}
