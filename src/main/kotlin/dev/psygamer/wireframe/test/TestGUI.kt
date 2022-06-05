549ae21ceebb390e65a3dd68fe77cf0b4909fe9e
psyGamer <psygamer21@gmail.com>
2022-06-05 23:05:00 +0200

    Moved `Button` & `Text` test widgets into `TestGUI`

--- src/main/kotlin/dev/psygamer/wireframe/test/TestGUI.kt
+++ src/main/kotlin/dev/psygamer/wireframe/test/TestGUI.kt
@@ -1,13 +1,41 @@
 package dev.psygamer.wireframe.test
 
+import net.minecraft.client.Minecraft
+import dev.psygamer.wireframe.api.client.render.PoseStack
+import dev.psygamer.wireframe.api.client.screen.ScreenRenderHelper
 import dev.psygamer.wireframe.gui.GUI
 import dev.psygamer.wireframe.gui.widget.*
+import dev.psygamer.wireframe.nativeapi.mcNative
+import dev.psygamer.wireframe.util.*
 
 object TestGUI : GUI() {
 
 	override fun setup() {
-		Button(padding = 2) {
-			Text("Abc")
+		TestButton(padding = 2) {
+			TestText("Abc")
 		}
 	}
+}
+
+private class TestButton(private val padding: Int, children: () -> Unit) : Widget(children) {
+
+	override fun render(poseStack: PoseStack) {
+		ScreenRenderHelper.drawTexturedQuad(100, 0, 200, 100, 0, 0, 16, 16, Identifier(path = "textures/block/stone.png"))
+		children.render(poseStack)
+	}
+
+	override val width: Int
+		get() = children.width + padding * 2
+	override val height: Int
+		get() = children.height + padding * 2
+}
+
+private class TestText(val text: String) : Widget() {
+
+	override fun render(poseStack: PoseStack) {
+		Minecraft.getInstance().font.draw(poseStack.mcNative, text, 0.0f, 0.0f, Color.WHITE.mcNative)
+	}
+
+	override val width = Minecraft.getInstance().font.width(text)
+	override val height = Minecraft.getInstance().font.lineHeight
 }
--- src/main/kotlin/dev/psygamer/wireframe/test/TestScreen.kt
+++ src/main/kotlin/dev/psygamer/wireframe/test/TestScreen.kt
@@ -1,9 +1,7 @@
 package dev.psygamer.wireframe.test
 
-import dev.psygamer.wireframe.api.client.screen.Screen
-import dev.psygamer.wireframe.api.client.screen.ScreenRenderHelper
-import dev.psygamer.wireframe.util.Color
-import dev.psygamer.wireframe.util.Identifier
+import dev.psygamer.wireframe.api.client.screen.*
+import dev.psygamer.wireframe.util.*
 
 object TestScreen : Screen() {
 	
@@ -11,9 +9,9 @@ object TestScreen : Screen() {
 		ScreenRenderHelper.drawQuad(0, 0, 100, 100, Color.DARK_RED)
 		ScreenRenderHelper.drawTexturedQuad(100, 0, 200, 100, 0, 0, 16, 16, Identifier(path = "textures/block/stone.png"))
 		
-		ScreenRenderHelper.drawText("Text", 150, 150, Color.WHITE)
-		ScreenRenderHelper.drawCenteredText("Centered Text", 150, 175, Color.BLACK)
-		ScreenRenderHelper.drawTextWithShadow("Text with Shadow", 150, 200, Color.LIGHT_GRAY)
-		ScreenRenderHelper.drawCenteredTextWithShadow("Centered Text with Shadow", 150, 225, Color(235, 24, 92))
+		ScreenRenderHelper.drawText("TestText", 150, 150, Color.WHITE)
+		ScreenRenderHelper.drawCenteredText("Centered TestText", 150, 175, Color.BLACK)
+		ScreenRenderHelper.drawTextWithShadow("TestText with Shadow", 150, 200, Color.LIGHT_GRAY)
+		ScreenRenderHelper.drawCenteredTextWithShadow("Centered TestText with Shadow", 150, 225, Color(235, 24, 92))
 	}
 }
