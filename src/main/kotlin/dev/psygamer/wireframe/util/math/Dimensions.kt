dfee2eb173cdd5a032c5de982e00e1ce108622d2
psyGamer <psygamer21@gmail.com>
2022-06-22 15:44:22 +0200

    Added helper classes for object dimensions

---
+++ src/main/kotlin/dev/psygamer/wireframe/util/math/Dimensions.kt
@@ -0,0 +1,9 @@
+package dev.psygamer.wireframe.util.math
+
+data class Dimension2I(val width: Int, val height: Int)
+data class Dimension2F(val width: Float, val height: Float)
+data class Dimension2D(val width: Double, val height: Double)
+
+data class Dimension3I(val width: Int, val height: Int, val depth: Int)
+data class Dimension3F(val width: Float, val height: Float, val depth: Float)
+data class Dimension3D(val width: Double, val height: Double, val depth: Double)
