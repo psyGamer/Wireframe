63beade374d9dd9f17ec4d9573b5cdd93c8ea2f4
psyGamer <psygamer21@gmail.com>
2022-06-23 20:33:48 +0200

    Updated target syntax in `TestGUI`

--- src/main/kotlin/dev/psygamer/wireframe/test/TestGUI.kt
+++ src/main/kotlin/dev/psygamer/wireframe/test/TestGUI.kt
@@ -4,7 +4,7 @@ import net.minecraft.client.Minecraft
 import dev.psygamer.wireframe.api.client.render.PoseStack
 import dev.psygamer.wireframe.api.client.screen.ScreenRenderHelper
 import dev.psygamer.wireframe.gui.GUI
-import dev.psygamer.wireframe.gui.modifier.*
+import dev.psygamer.wireframe.gui.modifier.Modifier
 import dev.psygamer.wireframe.gui.widget.*
 import dev.psygamer.wireframe.nativeapi.mcNative
 import dev.psygamer.wireframe.util.Color
@@ -12,35 +12,120 @@ import dev.psygamer.wireframe.util.Color
 object TestGUI : GUI() {
 
 	override fun setup() {
-		TestButton(
-			modifier = Modifier
-				.align(Alignment.CENTER)
-				.margin(5)
-		) {
-			TestText(
-				text = "Abc",
-				modifier = Modifier.align(Alignment.TOP_RIGHT)
-			)
+//		TestButton {
+//			TestButton(
+//				color = Color.DARK_GRAY,
+//				modifier = Modifier
+////				.align(Alignment.CENTER)
+//					.margin(5)
+//					.width(100)
+//			) {
+//				TestText(
+//					text = "Abc",
+//					modifier = Modifier//.align(Alignment.TOP_RIGHT)
+//						.margin(10)
+//				)
+//			}
+//		}
+//		TestButton(
+//			text = "Click me!",
+//			modifier = Modifier
+//				.align(Alignment.CENTER)
+//				.padding(3)
+//		)
+		Button {
+			Text("Hello, world!")
 		}
 	}
 }
 
-private class TestButton(modifier: Modifier, children: () -> Unit) : Widget(modifier, children) {
+private class Stack(
+	modifier: Modifier? = null, children: () -> Unit,
+) : CanvasWidget(modifier, children) {
 
-	override fun render() {
-		ScreenRenderHelper.drawQuad(poseStack, children.width, children.height, Color.DARK_GRAY)
-		children.render()
+	override fun render(poseStack: PoseStack) {
+		children.forEach {
+			poseStack.push()
+			val dimensions = it.computeDimensions()
+			val posX = (contentWidth - dimensions.width) / 2
+			val posY = (contentHeight - dimensions.height) / 2
+			poseStack.translate(posX, posY, 0)
+			it.render(poseStack)
+			poseStack.pop()
+		}
 	}
 
 	override val contentWidth: Int
-		get() = children.width
+		get() = run {
+			var currentWidth = 0
+			children.forEach {
+				val dimensions = it.computeDimensions()
+				if (dimensions.width > currentWidth)
+					currentWidth = dimensions.width
+			}
+			currentWidth
+		}
 	override val contentHeight: Int
-		get() = children.height
+		get() = run {
+			var currentHeight = 0
+			children.forEach {
+				val dimensions = it.computeDimensions()
+				if (dimensions.height > currentHeight)
+					currentHeight = dimensions.height
+			}
+			currentHeight
+		}
+
 }
 
-private class TestText(val text: String, modifier: Modifier) : Widget(modifier) {
+private class Button(
+	modifier: Modifier? = null, children: () -> Unit,
+) : Widget(modifier, children) {
 
-	override fun render() {
+	private class ButtonBackground : Widget({ }) {
+
+		override fun render(poseStack: PoseStack) {
+			ScreenRenderHelper.drawQuad(poseStack, this.contentWidth, this.contentHeight)
+		}
+	}
+
+	override fun setup() {
+		Stack {
+			ButtonBackground()
+			children()
+		}
+	}
+}
+
+//private class TestButton(
+//	private val color: Color = Color.DARK_RED,
+//	modifier: Modifier? = null, children: () -> Unit,
+//) : Widget(modifier, children) {
+//
+//	override fun render() {
+//		ScreenRenderHelper.drawQuad(poseStack, children.width, children.height, color)
+//		children.render()
+//	}
+//
+//	override val contentWidth: Int
+//		get() = children.width
+//	override val contentHeight: Int
+//		get() = children.height
+//}
+
+//private class TestButton(
+//	private val color: Color = Color.DARK_RED,
+//	private val text: String, modifier: Modifier? = null,
+//) : Widget(modifier) {
+//
+//	override fun setup() {
+//		TestText(text = text)
+//	}
+//}
+//
+private class Text(val text: String, modifier: Modifier? = null) : CanvasWidget(modifier) {
+
+	override fun render(poseStack: PoseStack) {
 		Minecraft.getInstance().font.draw(poseStack.mcNative, text, 0.0f, 0.0f, Color.WHITE.mcNative)
 	}
 
