6e05abdcefbae7e65268d2ced637b8514f88b9be
psyGamer <psygamer21@gmail.com>
2022-07-01 15:55:00 +0200

    Fixed not using `get()` for content size

--- src/main/kotlin/dev/psygamer/wireframe/gui/widget/ParentWidget.kt
+++ src/main/kotlin/dev/psygamer/wireframe/gui/widget/ParentWidget.kt
@@ -16,8 +16,8 @@ abstract class ParentWidget(
 
 	override fun render(poseStack: PoseStack) = children.forEach { it.render(poseStack) }
 
-	override val contentWidth = if (children.isEmpty()) 0 else children.maxOf { it.width }
-	override val contentHeight = if (children.isEmpty()) 0 else children.maxOf { it.height }
+	override val contentWidth get() = if (children.isEmpty()) 0 else children.maxOf { it.width }
+	override val contentHeight get() = if (children.isEmpty()) 0 else children.maxOf { it.height }
 
 	internal fun compileChildren() {
 		this.children = WidgetCompiler.compileWidgets(this, childrenFn)
