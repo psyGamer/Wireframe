70350711a0682df32aa224d77c16b292692cc46f
psyGamer <psygamer21@gmail.com>
2022-07-13 14:52:51 +0200

    Switched to using new `RenderingContext`

--- src/main/kotlin/dev/psygamer/wireframe/api/client/RenderHelper3D.kt
+++ src/main/kotlin/dev/psygamer/wireframe/api/client/RenderHelper3D.kt
@@ -1,52 +1,56 @@
 package dev.psygamer.wireframe.api.client
 
-import dev.psygamer.wireframe.api.client.render.RenderingContext
 import dev.psygamer.wireframe.api.item.ItemStack
 import dev.psygamer.wireframe.nativeapi.client.NativeRenderHelper3D
+import dev.psygamer.wireframe.nativeapi.client.render.context.RenderingContext
 import dev.psygamer.wireframe.util.Color
 
 object RenderHelper3D {
-	
+
 	/**
 	 * Renders text with a drop shadow.
 	 */
 	fun renderText(
 		context: RenderingContext, text: String, textColor: Color = Color.WHITE, backgroundColor: Color = Color.TRANSPARENT,
 		renderRightToLeft: Boolean = false,
-	) =
-		NativeRenderHelper3D.renderText(context, text, textColor, backgroundColor,
-										dropShadow = true, centered = false, renderRightToLeft)
-	
+	) = NativeRenderHelper3D.renderText(
+		context, text, textColor, backgroundColor,
+		dropShadow = true, centered = false, renderRightToLeft
+	)
+
 	/**
 	 * Renders centered text with a drop shadow.
 	 */
 	fun renderCenteredText(
 		context: RenderingContext, text: String, textColor: Color = Color.WHITE, backgroundColor: Color = Color.TRANSPARENT,
 		renderRightToLeft: Boolean = false,
-	) =
-		NativeRenderHelper3D.renderText(context, text, textColor, backgroundColor,
-										dropShadow = true, centered = true, renderRightToLeft)
-	
+	) = NativeRenderHelper3D.renderText(
+		context, text, textColor, backgroundColor,
+		dropShadow = true, centered = true, renderRightToLeft
+	)
+
 	/**
 	 * Renders text without a drop shadow.
 	 */
 	fun renderTextNoShadow(
 		context: RenderingContext, text: String, textColor: Color = Color.WHITE, backgroundColor: Color = Color.TRANSPARENT,
 		renderRightToLeft: Boolean = false,
-	) =
-		NativeRenderHelper3D.renderText(context, text, textColor, backgroundColor,
-										dropShadow = false, centered = false, renderRightToLeft)
-	
+	) = NativeRenderHelper3D.renderText(
+		context, text, textColor, backgroundColor,
+		dropShadow = false, centered = false, renderRightToLeft
+	)
+
 	/**
 	 * Renders centered text without a drop shadow.
 	 */
 	fun renderCenteredTextNoShadow(
 		context: RenderingContext, text: String, textColor: Color = Color.WHITE, backgroundColor: Color = Color.TRANSPARENT,
 		renderRightToLeft: Boolean = false,
-	) =
-		NativeRenderHelper3D.renderText(context, text, textColor, backgroundColor,
-										dropShadow = false, centered = true, renderRightToLeft)
-	
+	) = NativeRenderHelper3D.renderText(
+		context, text, textColor, backgroundColor,
+		dropShadow = false, centered = true, renderRightToLeft
+	)
+
 	/**
 	 * Renders a dropped [ItemStack]
 	 */
--- src/main/kotlin/dev/psygamer/wireframe/nativeapi/client/NativeRenderHelper3D.kt
+++ src/main/kotlin/dev/psygamer/wireframe/nativeapi/client/NativeRenderHelper3D.kt
@@ -3,54 +3,37 @@ package dev.psygamer.wireframe.nativeapi.client
 import net.minecraft.client.Minecraft
 import net.minecraft.client.renderer.model.ItemCameraTransforms
 import net.minecraft.client.renderer.texture.OverlayTexture
-import dev.psygamer.wireframe.api.client.render.RenderingContext
 import dev.psygamer.wireframe.api.item.ItemStack
+import dev.psygamer.wireframe.nativeapi.client.render.context.RenderingContext
 import dev.psygamer.wireframe.nativeapi.mcNative
 import dev.psygamer.wireframe.util.Color
-import dev.psygamer.wireframe.util.helper.using
 
 object NativeRenderHelper3D {
-	
+
 	fun renderText(
 		context: RenderingContext, text: String, textColor: Color, backgroundColor: Color,
 		dropShadow: Boolean, centered: Boolean, rightToLeft: Boolean,
 	) {
 		val font = Minecraft.getInstance().font
-		
-		using(context.poseStack.push()) {
-			context.poseStack.scale(-1 / 16.0f, -1 / 16.0f, -1 / 16.0f) // Ensure that 1 font pixel = 1 block pixel
-			
-			context.poseStack.rotate(context.rotation)
-			context.poseStack.scale(context.scale)
-			
-			val transparent = textColor.alpha < 1.0f
-			val xOffset = if (centered) font.width(text) / -2.0f else 0.0f
-			// FIXME: Ba
-			font.drawInBatch(text, xOffset, 0.0f, textColor.mcNative, dropShadow, context.poseStack.mcNative.last().pose(),
-							 context.renderBuffer, transparent, backgroundColor.mcNative, context.packedLight, rightToLeft)
-		}
+
+		val transparent = textColor.alpha < 1.0f
+		val xOffset = if (centered) font.width(text) / -2.0f else 0.0f
+
+		font.drawInBatch(
+			text, xOffset, 0.0f, textColor.mcNative, dropShadow, context.poseStack.mcNative.last().pose(),
+			context.renderTypeBuffer, transparent, backgroundColor.mcNative, context.packedLightmap, rightToLeft
+		)
 	}
-	
+
 	fun renderItemStack(context: RenderingContext, itemStack: ItemStack) {
 		val itemRenderer = Minecraft.getInstance().itemRenderer
 		val itemModel = itemRenderer.getModel(itemStack.mcNative, null, null)
-		
-		using(context.poseStack.push()) {
-			context.poseStack.rotate(context.rotation)
-			context.poseStack.scale(context.scale)
-			
-			itemRenderer.render(itemStack.mcNative, ItemCameraTransforms.TransformType.GROUND, true, context.poseStack.mcNative,
-								context.renderBuffer, context.packedLight, OverlayTexture.NO_OVERLAY, itemModel)
-		}
+
+		itemRenderer.render(
+			itemStack.mcNative, ItemCameraTransforms.TransformType.GROUND, true, context.poseStack.mcNative,
+			context.renderTypeBuffer, context.packedLightmap, OverlayTexture.NO_OVERLAY, itemModel
+		)
 	}
-	
-	/**
-	 * Packs sky and block light into a single integer.
-	 * ```
-	 * Format: 00000000 11110000 00000000 11110000
-	 *                     ^ Sky             ^ Block
-	 * ```
-	 */
-	private val RenderingContext.packedLight
-		get() = this.skyLight shr 20 or this.blockLight shr 4
+
+	fun packedLight(skyLight: Int, blockLight: Int) = skyLight shr 20 or blockLight shr 4
 }
